a3bf3facd57918be5300a331d971963d625ec63f
SilvioCresci <silviocresci1996@gmail.com>
2018-12-31 12:19:18 +0100

    feat: aggiunta Documenti.java

---
+++ src/main/java/it/unisa/ErasmusTracking/bean/Documenti.java
@@ -0,0 +1,81 @@
+package main.java.it.unisa.ErasmusTracking.bean;
+
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.util.Date;
+
+public class Documenti{
+
+    private int id;
+    private String nome;
+    private DateFormat data;
+    private String dataString;
+    private Date data_caricamento;
+    private String url;
+    private int proprietario;
+
+
+    public Documenti(int id, String nome, String data_caricamento, String url, int proprietario){
+        this.id = id;
+        this.nome = nome;
+        dataString = data_caricamento;
+        data = DateFormat.getDateInstance(DateFormat.SHORT);
+        try {
+            this.data_caricamento=data.parse(dataString);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        this.url = url;
+        this.proprietario = proprietario;
+    }
+
+    public Documenti() {
+
+    }
+
+    public int getId(){
+        return id;
+    }
+
+    public String getNome(){
+        return nome;
+    }
+
+    public String getDataCaricamento(){
+        return data.format(data_caricamento);
+    }
+
+    public String getUrl(){
+        return url;
+    }
+
+    public int getProprietario(){
+        return proprietario;
+    }
+
+    public void setId(int newId) {
+        id=newId;
+    }
+
+    public void setNome(String newNome) {
+        nome=newNome;
+    }
+
+    public void setDataCaricamento(String newData) {
+        dataString=newData;
+        data = DateFormat.getDateInstance(DateFormat.SHORT);
+        try {
+            this.data_caricamento=data.parse(dataString);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void setUrl(String newUrl) {
+        url=newUrl;
+    }
+
+    public void setProprietario(int newProprietario) {
+        proprietario=newProprietario;
+    }
+}
