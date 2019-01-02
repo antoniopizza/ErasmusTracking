accea74990646ca7215a0bc8f6c7e4d70d03be49
DOGER92 <45942667+DOGER92@users.noreply.github.com>
2019-01-02 10:40:59 +0100

    feat: Modifica Bean

--- src/main/java/it/unisa/ErasmusTracking/bean/Amministratore.java
+++ src/main/java/it/unisa/ErasmusTracking/bean/Amministratore.java
@@ -1,45 +1,57 @@
 package main.java.it.unisa.ErasmusTracking.bean;
 
-public class Amministratore extends Account {
+public class Amministratore extends Account
+{
 
 
-    public Amministratore(int id, String nome, String cognome, String email, String password) {
+    public Amministratore(int id, String nome, String cognome, String email, String password)
+    {
         super(id, nome, cognome, email, password);
     }
 
-    public String getNome() {
+    public String getNome()
+    {
         return super.getNome();
     }
 
-    public void setNome(String nome) {
+    public void setNome(String nome)
+    {
         super.setNome(nome);
     }
 
-    public String getCognome(){
+    public String getCognome()
+    {
         return super.getCognome();
+
     }
 
-    public void setCognome (String cognome) {
+    public void setCognome (String cognome)
+    {
         super.setCognome(cognome);
     }
 
-    public String getEmail() {
+    public String getEmail()
+    {
         return super.getEmail();
     }
 
-    public void setEmail(String email) {
+    public void setEmail(String email)
+    {
         super.setEmail(email);
     }
 
-    public String getPassword() {
+    public String getPassword()
+    {
         return super.getPassword();
     }
 
-    public void setPassword(String password) {
+    public void setPassword(String password)
+    {
         super.setPassword(password);
     }
 
-    public String toString() {
+    public String toString()
+    {
         return super.toString()+ " Ruolo = Amministratore";
     }
 }
