52834ef2eb8e628834637b3606b836f117af4b8f
Dario Scola <scoladario@hotmail.it>
2019-01-02 13:26:23 +0100

    feat: confirmed ticket's bean and added ITicketDao

--- src/main/java/it/unisa/ErasmusTracking/model/dao/ITicketDao.java
+++ src/main/java/it/unisa/ErasmusTracking/model/dao/ITicketDao.java
@@ -5,5 +5,7 @@ import main.java.it.unisa.ErasmusTracking.bean.Ticket;
 import java.util.Collection;
 
 public interface ITicketDao extends IGenericDao {
+    public Collection<Ticket> doRetrieveByIdAccount(int idAccount);
+    public Collection<Ticket>  doRetrieveByUsernameStudent(String username);
 
 }
