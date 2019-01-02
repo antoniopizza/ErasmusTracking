fa2691bab66d277d86fde0e9fd41d3c5530ffa8b
DOGER92 <45942667+DOGER92@users.noreply.github.com>
2019-01-02 11:46:46 +0100

    feat: Modifica Bean

--- src/main/java/it/unisa/ErasmusTracking/bean/Studente.java
+++ src/main/java/it/unisa/ErasmusTracking/bean/Studente.java
@@ -1,8 +1,7 @@
 package main.java.it.unisa.ErasmusTracking.bean;
 import java.util.Date;
 
-public class Studente extends Account
-{
+public class Studente extends Account {
     private String sesso;
     private Date dataDiNascita;
     private String luogoDiNascita;
@@ -17,8 +16,7 @@ public class Studente extends Account
     public Studente(int id, String nome, String cognome, String email,
                     String password, String sesso, Date dataDiNascita,
                     String luogoDiNascita, String nazionalita, String cicloDiStudi,
-                    String codiceMateria, String telefono, String annoAccademico)
-    {
+                    String codiceMateria, String telefono, String annoAccademico) {
         super(id, nome, cognome, email, password);
         this.sesso = sesso;
         this.dataDiNascita = dataDiNascita;
@@ -29,148 +27,121 @@ public class Studente extends Account
         this.telefono = telefono;
         this.annoAccademico = annoAccademico;
     }
-    public int getId()
-    {
+
+    public int getId() {
         return super.getId();
     }
 
-    public void setId(int id)
-    {
+    public void setId(int id) {
         super.setId(id);
     }
 
-    public String getNome()
-    {
+    public String getNome() {
         return super.getNome();
     }
 
-    public void setNome(String nome)
-    {
+    public void setNome(String nome) {
         super.setNome(nome);
     }
 
-    public String getCognome()
-    {
+    public String getCognome() {
         return super.getCognome();
     }
 
-    public void setCognome (String cognome)
-    {
+    public void setCognome(String cognome) {
         super.setCognome(cognome);
     }
 
-    public String getEmail()
-    {
+    public String getEmail() {
         return super.getEmail();
     }
 
-    public void setEmail(String email)
-    {
+    public void setEmail(String email) {
         super.setEmail(email);
     }
 
-    public String getPassword()
-    {
+    public String getPassword() {
         return super.getPassword();
     }
 
-    public void setPassword(String password)
-    {
+    public void setPassword(String password) {
         super.setPassword(password);
     }
 
-    public String getSesso()
-    {
+    public String getSesso() {
         return sesso;
     }
 
-    public void setSesso(String sesso)
-    {
+    public void setSesso(String sesso) {
         this.sesso = sesso;
     }
 
-    public Date getDataDiNascita()
-    {
+    public Date getDataDiNascita() {
         return dataDiNascita;
     }
 
-    public void setDataDiNascita(Date dataDiNascita)
-    {
+    public void setDataDiNascita(Date dataDiNascita) {
         this.dataDiNascita = dataDiNascita;
     }
 
-    public String getLuogoDiNascita()
-    {
+    public String getLuogoDiNascita() {
         return luogoDiNascita;
     }
 
-    public void setLuogoDiNascita(String luogoDiNascita)
-    {
+    public void setLuogoDiNascita(String luogoDiNascita) {
         this.luogoDiNascita = luogoDiNascita;
     }
 
-    public String getNazionalita()
-    {
+    public String getNazionalita() {
         return nazionalita;
     }
 
-    public void setNazionalita(String nazionalita)
-    {
+    public void setNazionalita(String nazionalita) {
         this.nazionalita = nazionalita;
     }
 
-    public String getCicloDiStudi()
-    {
+    public String getCicloDiStudi() {
         return cicloDiStudi;
     }
 
-    public void setCicloDiStudi(String cicloDiStudi)
-    {
+    public void setCicloDiStudi(String cicloDiStudi) {
         this.cicloDiStudi = cicloDiStudi;
     }
 
-    public String getCodiceMateria()
-    {
+    public String getCodiceMateria() {
         return codiceMateria;
     }
 
-    public void setCodiceMateria(String codiceMateria)
-    {
+    public void setCodiceMateria(String codiceMateria) {
         this.codiceMateria = codiceMateria;
     }
 
-    public String getTelefono()
-    {
+    public String getTelefono() {
         return telefono;
     }
 
-    public void setTelefono(String telefono)
-    {
+    public void setTelefono(String telefono) {
         this.telefono = telefono;
     }
 
-    public String getAnnoAccademico()
-    {
+    public String getAnnoAccademico() {
         return annoAccademico;
     }
 
-    public void setAnnoAccademico(String annoAccademico)
-    {
+    public void setAnnoAccademico(String annoAccademico) {
         this.annoAccademico = annoAccademico;
     }
 
-    public String getMatricola()
-    {
+    public String getMatricola() {
         return matricola;
     }
 
-    public void setMatricola(String matricola)
-    {
+    public void setMatricola(String matricola) {
         this.matricola = matricola;
     }
 
-    public String toString()
-    {
+    @Override
+    public String toString() {
         return super.toString() + "\n" +
                 "Sesso= " + sesso + "\n" +
                 "Data di nascita= " + dataDiNascita + "\n" +
@@ -181,5 +152,48 @@ public class Studente extends Account
                 "Telefono= " + telefono + "\n" +
                 "Anno accademico= " + annoAccademico + "\n" +
                 "Matricola= " + matricola + "\n";
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == this) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (this.getClass() != obj.getClass())
+            return false;
+
+        Studente adm = (Studente) obj;
+        return this.equals(adm);
+    }
+
+    @Override
+    protected Object clone() throws CloneNotSupportedException {
+        try {
+            Studente b = (Studente) super.clone();
+
+            b.setId(this.getId());
+            b.setNome(this.getNome());
+            b.setCognome(this.getCognome());
+            b.setEmail(this.getEmail());
+            b.setPassword(this.getPassword());
+            b.setSesso(this.getSesso());
+            b.setDataDiNascita(this.getDataDiNascita());
+            b.setLuogoDiNascita(this.getLuogoDiNascita());
+            b.setNazionalita(this.getNazionalita());
+            b.setCicloDiStudi(this.setCicloDiStudi());
+            b.setCodiceMateria(this.getCodiceMateria());
+            b.setTelefono(this.getTelefono());
+            b.setAnnoAccademico(this.getAnnoAccademico());
+
+            return b;
+        } catch (CloneNotSupportedException e) {
+            e.printStackTrace();
+
+            return null;
+        }
+
     }
 }
