472a13d717f09a445ed91c4a41b79ffb2efcee8c
Alexert97 <38686550+Alexert97@users.noreply.github.com>
2019-01-03 10:52:54 +0100

    Revert "Merge branch 'develop' of https://github.com/antoniopizza/ErasmusTracking into develop"
    This reverts commit 6d4bdb99eff1453b3834b0919d59379b7fff6dd3, reversing
    changes made to eeac990a71dcedf8fe9145866f4e832d15071fd1.

--- src/main/java/it/unisa/ErasmusTracking/model/jpa/MappingEsameManager.java
+++ src/main/java/it/unisa/ErasmusTracking/model/jpa/MappingEsameManager.java
@@ -9,6 +9,8 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.LinkedList;
 import java.util.List;
 
 
--- src/main/java/it/unisa/ErasmusTracking/model/jpa/ReceivingInstituteManager.java
+++ src/main/java/it/unisa/ErasmusTracking/model/jpa/ReceivingInstituteManager.java
@@ -8,6 +8,7 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.Collection;
 import java.util.LinkedList;
 import java.util.List;
 
