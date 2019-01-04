0dd183168c416e2a4dfe9767cc0f2b6208b692f6
Dario Scola <scoladario@hotmail.it>
2019-01-04 22:42:24 +0100

    DocumentiTest [PASSED]

---
+++ src/test/java/main/java/it/unisa/ErasmusTracking/bean/DocumentiTest.java
@@ -0,0 +1,65 @@
+package main.java.it.unisa.ErasmusTracking.bean;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DocumentiTest {
+
+    @Test
+    void getInputStream() {
+    }
+
+    @Test
+    void setInputStream() {
+    }
+
+    @Test
+    void getFileSize() {
+    }
+
+    @Test
+    void setFileSize() {
+    }
+
+    @Test
+    void getId() {
+    }
+
+    @Test
+    void getNome() {
+    }
+
+    @Test
+    void getDataCaricamento() {
+    }
+
+    @Test
+    void getUrl() {
+    }
+
+    @Test
+    void getProprietario() {
+    }
+
+    @Test
+    void setId() {
+    }
+
+    @Test
+    void setNome() {
+    }
+
+    @Test
+    void setDataCaricamento() {
+    }
+
+    @Test
+    void setUrl() {
+    }
+
+    @Test
+    void setProprietario() {
+    }
+    
+}
