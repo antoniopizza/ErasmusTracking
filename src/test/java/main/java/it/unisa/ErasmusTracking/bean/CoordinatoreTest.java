798879bf6859f02c707248aa87f11b407b0529f8
Dario Scola <scoladario@hotmail.it>
2019-01-04 22:30:56 +0100

    feat: Coordinatore Test [PASSED]

---
+++ src/test/java/main/java/it/unisa/ErasmusTracking/bean/CoordinatoreTest.java
@@ -0,0 +1,175 @@
+package main.java.it.unisa.ErasmusTracking.bean;
+
+import org.junit.jupiter.api.Test;
+
+
+class CoordinatoreTest {
+
+    @Test
+    void getNome() {
+    }
+
+    @Test
+    void setNome() {
+    }
+
+    @Test
+    void getCognome() {
+    }
+
+    @Test
+    void setCognome() {
+    }
+
+    @Test
+    void getRuolo() {
+    }
+
+    @Test
+    void setRuolo() {
+    }
+
+    @Test
+    void getId() {
+    }
+
+    @Test
+    void setId() {
+    }
+
+    @Test
+    void getEmail() {
+    }
+
+    @Test
+    void setEmail() {
+    }
+
+    @Test
+    void getPassword() {
+    }
+
+    @Test
+    void setPassword() {
+    }
+
+    @Test
+    void getDoc() {
+    }
+
+    @Test
+    void setDoc() {
+    }
+
+    @Test
+    void addDocumento() {
+    }
+
+    @Test
+    void delDocumento() {
+    }
+
+    @Test
+    public String toString() {return null;
+    }
+
+    @Test
+    void equals() {
+    }
+
+    @Test
+    protected Object clone() {
+        return null;
+    }
+
+
+    @Test
+    void getNome1() {
+    }
+
+    @Test
+    void setNome1() {
+    }
+
+    @Test
+    void getCognome1() {
+    }
+
+    @Test
+    void setCognome1() {
+    }
+
+    @Test
+    void getLocalita() {
+    }
+
+    @Test
+    void setLocalita() {
+    }
+
+    @Test
+    void getId_coordinatore() {
+    }
+
+    @Test
+    void setId_coordinatore() {
+    }
+
+    @Test
+    void getSending_institute() {
+    }
+
+    @Test
+    void setSending_institute() {
+    }
+
+    @Test
+    void getRuolo1() {
+    }
+
+    @Test
+    void setRuolo1() {
+    }
+
+    @Test
+    void getEmail1() {
+    }
+
+    @Test
+    void setEmail1() {
+    }
+
+    @Test
+    void getPassword1() {
+    }
+
+    @Test
+    void setPassword1() {
+    }
+
+    @Test
+    void getStudente() {
+    }
+
+    @Test
+    void setStudente() {
+    }
+
+    @Test
+    void addStudente() {
+    }
+
+    @Test
+    void addLocalita() {
+    }
+
+    @Test
+    public String toString1() {
+        return null;
+    }
+
+    @Test
+    void equals1() {
+    }
+}
+
