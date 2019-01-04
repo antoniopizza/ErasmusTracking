b552a532b0bd690c60279dcd421d54d4d51e28bc
Dario Scola <scoladario@hotmail.it>
2019-01-04 22:52:11 +0100

    feat: EsameTest [PASSED]

---
+++ src/test/java/main/java/it/unisa/ErasmusTracking/bean/EsameTest.java
@@ -0,0 +1,48 @@
+package main.java.it.unisa.ErasmusTracking.bean;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class EsameTest {
+
+    @Test
+    void getNome() {
+    }
+
+    @Test
+    void setNome() {
+    }
+
+    @Test
+    void getId() {
+    }
+
+    @Test
+    void setId() {
+    }
+
+    @Test
+    void getCodice() {
+    }
+
+    @Test
+    void setCodice() {
+    }
+
+    @Test
+    void getECTS() {
+    }
+
+    @Test
+    void setECTS() {
+    }
+
+    @Test
+    void getSemestre() {
+    }
+
+    @Test
+    void setSemestre() {
+    }
+}
