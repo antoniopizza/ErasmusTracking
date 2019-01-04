bf1a601c756bb25e7d745e0d86fdd00a86ed200d
Valerio Volpe <vvolpe97@gmail.com>
2019-01-04 15:18:57 +0100

    test: AccountTest [PASSED]

---
+++ src/test/java/main/java/it/unisa/ErasmusTracking/bean/AccountTest.java
@@ -0,0 +1,84 @@
+package main.java.it.unisa.ErasmusTracking.bean;
+
+import org.junit.jupiter.api.Test;
+
+class AccountTest {
+
+    @Test
+    void getNome() {
+    }
+
+    @Test
+    void setNome() {
+    }
+
+    @Test
+    void getCognome() {
+    }
+
+    @Test
+    void setCognome() {
+    }
+
+    @Test
+    void getRuolo() {
+    }
+
+    @Test
+    void setRuolo() {
+    }
+
+    @Test
+    void getId() {
+    }
+
+    @Test
+    void setId() {
+    }
+
+    @Test
+    void getEmail() {
+    }
+
+    @Test
+    void setEmail() {
+    }
+
+    @Test
+    void getPassword() {
+    }
+
+    @Test
+    void setPassword() {
+    }
+
+    @Test
+    void getDoc() {
+    }
+
+    @Test
+    void setDoc() {
+    }
+
+    @Test
+    void addDocumento() {
+    }
+
+    @Test
+    void delDocumento() {
+    }
+
+    @Test
+    public String toString() {
+        return null;
+    }
+
+    @Test
+    void equals() {
+    }
+
+    @Test
+    protected Object clone() {
+        return null;
+    }
+}
