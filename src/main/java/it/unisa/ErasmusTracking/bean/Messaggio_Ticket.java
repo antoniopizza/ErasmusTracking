39b8f1cf9c1d3c86b71b9089b56151eed73662a2
DOGER92 <45942667+DOGER92@users.noreply.github.com>
2019-01-05 15:47:53 +0100

    feat: add method
    -Messaggio ticket: aggiunta toString,equals e clone

--- src/main/java/it/unisa/ErasmusTracking/bean/Messaggio_Ticket.java
+++ src/main/java/it/unisa/ErasmusTracking/bean/Messaggio_Ticket.java
@@ -5,7 +5,8 @@ import java.text.ParseException;
 import java.util.*;
 
 
-public class Messaggio_Ticket {
+public class Messaggio_Ticket
+{
     private int id_messaggio;
     private String contenuto;
     private String data_invio;
@@ -14,7 +15,8 @@ public class Messaggio_Ticket {
     private int proprietario; // account(id_account)
 
 
-    public Messaggio_Ticket(int id_messaggio, String contenuto, String data_invio, String ora_invio, int ticket_id, int proprietario) {
+    public Messaggio_Ticket(int id_messaggio, String contenuto, String data_invio, String ora_invio, int ticket_id, int proprietario)
+    {
         this.id_messaggio = id_messaggio;
         this.contenuto = contenuto;
         this.data_invio = data_invio;
@@ -23,7 +25,8 @@ public class Messaggio_Ticket {
         this.ora_invio = ora_invio;
     }
 
-    public Messaggio_Ticket(){
+    public Messaggio_Ticket()
+    {
     }
         public int getId_messaggio () {return id_messaggio;}
         public String getContenuto () {return contenuto;}
@@ -41,5 +44,73 @@ public class Messaggio_Ticket {
         }
         public void setTicket_id(int NewId_Ticket){ticket_id=NewId_Ticket;}
         public void setProprietario(int NewProprietario){proprietario=NewProprietario;}
+
+
+    @Override
+    public String toString()
+    {
+        return "id del messaggio= " + id_messaggio + "\n"+
+                "Contenuto= "+ contenuto+ "\n"+
+                "Data di Invio= "+ data_invio+ "\n"+
+                "Ora di Invio= "+ data_invio+ "\n"+
+                "Id del ticket= " + ticket_id+ "\n" +
+                "Proprietario= " + proprietario+ "\n";
     }
+
+
+    @Override
+    public boolean equals(Object obj)
+    {
+        if(obj == this)
+        {
+            return true;
+        }
+        if (obj == null)
+        {
+            return false;
+        }
+        if (this.getClass() != obj.getClass())
+            return false;
+
+        Messaggio_Ticket acc = (Messaggio_Ticket) obj;
+
+        if(this.getId_messaggio()==(acc.getId_messaggio())&&
+                this.getContenuto().equals(acc.getContenuto()) &&
+                this.getDataInvio().equals(acc.getDataInvio())&&
+                this.getOraInvio().equals(acc.getOraInvio()) &&
+                this.getTicket_id()==acc.getTicket_id()&&
+                this.getProprietario()==acc.getProprietario())
+        {
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    protected Object clone() throws CloneNotSupportedException
+    {
+        try
+        {
+
+            Messaggio_Ticket b = (Messaggio_Ticket) super.clone();
+
+            b.setId_messaggio(this.getId_messaggio());
+            b.setContenuto(this.getContenuto());
+            b.setData_invio(this.getDataInvio());
+            b.setOra_invio(this.getOraInvio());
+            b.setTicket_id(this.getTicket_id());
+            b.setProprietario(this.getProprietario());
+
+            return b;
+        }
+
+        catch(CloneNotSupportedException e)
+        {
+            e.printStackTrace();
+
+            return null;
+        }
+
+    }
+}
 
