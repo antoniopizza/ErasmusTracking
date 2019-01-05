c8c1a3e5496e5f10d7c66b0b4a9e3a0c2f0f4a7d
DOGER92 <45942667+DOGER92@users.noreply.github.com>
2019-01-05 16:20:16 +0100

    feat: method add
    -SendingInstitute: aggiunta dei metodi toString,equals e clone

--- src/main/java/it/unisa/ErasmusTracking/bean/SendingInstitute.java
+++ src/main/java/it/unisa/ErasmusTracking/bean/SendingInstitute.java
@@ -46,4 +46,65 @@ public class SendingInstitute {
     public void setDipartimento(String dipartimento) {
         this.dipartimento = dipartimento;
     }
+
+    @Override
+    public String toString()
+    {
+        return "id= " + id + "\n"+
+                "Codice Erasmus= "+ codiceErasmus+ "\n"+
+                "Indirizzo Istitute= "+ indirizzo + "\n"+
+                "Dipartimento= "+ dipartimento + "\n";
+    }
+
+
+    @Override
+    public boolean equals(Object obj)
+    {
+        if(obj == this)
+        {
+            return true;
+        }
+        if (obj == null)
+        {
+            return false;
+        }
+        if (this.getClass() != obj.getClass())
+            return false;
+
+        SendingInstitute acc = (SendingInstitute) obj;
+
+        if(this.getId()==(acc.getId())&&
+                this.getCodiceErasmus().equals(acc.getCodiceErasmus()) &&
+                this.getIndirizzo().equals(acc.getIndirizzo())&&
+                this.getDipartimento().equals(acc.getDipartimento()))
+        {
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    protected Object clone() throws CloneNotSupportedException
+    {
+        try
+        {
+
+            SendingInstitute b = (SendingInstitute) super.clone();
+
+            b.setId(this.getId());
+            b.setCodiceErasmus(this.getCodiceErasmus());
+            b.setIndirizzo(this.getIndirizzo());
+            b.setDipartimento(this.getDipartimento());
+
+            return b;
+        }
+
+        catch(CloneNotSupportedException e)
+        {
+            e.printStackTrace();
+
+            return null;
+        }
+
+    }
 }
