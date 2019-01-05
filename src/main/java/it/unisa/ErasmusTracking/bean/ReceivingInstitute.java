383f66976799eb5110f322e0743e43c9ca69d6a8
DOGER92 <45942667+DOGER92@users.noreply.github.com>
2019-01-05 16:14:35 +0100

    feat: method add
    -ReceivingInstitute: aggiunta dei metodi toString,equals e clone

--- src/main/java/it/unisa/ErasmusTracking/bean/ReceivingInstitute.java
+++ src/main/java/it/unisa/ErasmusTracking/bean/ReceivingInstitute.java
@@ -90,4 +90,80 @@ public class ReceivingInstitute {
     public void setWebsite(String website) {
         this.website = website;
     }
+
+    @Override
+    public String toString()
+    {
+        return "id= " + id + "\n"+
+                "Località= "+ localita + "\n"+
+                "Nome del Contatto= "+ nomeContatto+ "\n"+
+                "E-mail del contatto= "+ emailContatto+ "\n"+
+                "Grandezza dell'istituto= " + sizeOfEnterprise+ "\n" +
+                "Nome del mentore= " + nomeMentore + "\n"+
+                "E-mail del mentore= "+ emailMentore + "\n"+
+                "Sito web= "+ website + "\n"+
+                "Codice Erasmus= "+ codiceErasmus+ "\n";
+    }
+
+
+    @Override
+    public boolean equals(Object obj)
+    {
+        if(obj == this)
+        {
+            return true;
+        }
+        if (obj == null)
+        {
+            return false;
+        }
+        if (this.getClass() != obj.getClass())
+            return false;
+
+        ReceivingInstitute acc = (ReceivingInstitute) obj;
+
+        if(this.getId()==(acc.getId())&&
+                this.getLocalita()==acc.getLocalita() &&
+                this.getNomeContatto().equals(acc.getNomeContatto())&&
+                this.getEmailContatto().equals(acc.getEmailContatto()) &&
+                this.getSizeOfEnterprise().equals(acc.getSizeOfEnterprise())&&
+                this.getNomeMentore().equals(acc.getNomeMentore()) &&
+                this.getEmailMentore().equals(acc.getEmailMentore())&&
+                this.getWebsite().equals(acc.getWebsite()) &&
+                this.getCodiceErasmus().equals(acc.getCodiceErasmus()))
+        {
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    protected Object clone() throws CloneNotSupportedException
+    {
+        try
+        {
+
+            ReceivingInstitute b = (ReceivingInstitute) super.clone();
+
+            b.setId(this.getId());
+            b.setLocalita(this.getLocalita());
+            b.setNomeContatto(this.getNomeContatto());
+            b.setEmailContatto(this.getEmailContatto());
+            b.setSizeOfEnterprise(this.getSizeOfEnterprise());
+            b.setNomeMentore(this.getNomeMentore());
+            b.setEmailMentore(this.getEmailMentore());
+            b.setWebsite(this.getWebsite());
+            b.setCodiceErasmus(this.getCodiceErasmus());
+
+            return b;
+        }
+
+        catch(CloneNotSupportedException e)
+        {
+            e.printStackTrace();
+
+            return null;
+        }
+
+    }
 }
