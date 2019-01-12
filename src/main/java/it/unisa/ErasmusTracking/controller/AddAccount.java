c0da3af95b5034dd1f80d4b37e6b2eb0385ae2f4
Alexert97 <38686550+Alexert97@users.noreply.github.com>
2019-01-12 11:48:16 +0100

    checkstyle: Account

--- src/main/java/it/unisa/ErasmusTracking/controller/AddAccount.java
+++ src/main/java/it/unisa/ErasmusTracking/controller/AddAccount.java
@@ -1,23 +1,6 @@
 package main.java.it.unisa.ErasmusTracking.controller;
 
-
-
-import main.java.it.unisa.ErasmusTracking.bean.Account;
-import main.java.it.unisa.ErasmusTracking.bean.Documenti;
-import main.java.it.unisa.ErasmusTracking.bean.Studente;
-import main.java.it.unisa.ErasmusTracking.model.dao.IAccountDao;
-import main.java.it.unisa.ErasmusTracking.model.dao.IDocumentoDao;
-import main.java.it.unisa.ErasmusTracking.model.dao.ILocalitaDao;
-import main.java.it.unisa.ErasmusTracking.model.dao.IStudenteDao;
-import main.java.it.unisa.ErasmusTracking.model.jpa.AccountManager;
-import main.java.it.unisa.ErasmusTracking.model.jpa.DocumentiManager;
-import main.java.it.unisa.ErasmusTracking.model.jpa.LocalitaManager;
-import main.java.it.unisa.ErasmusTracking.model.jpa.StudenteManager;
-
 import java.io.IOException;
-import java.sql.SQLException;
-
-
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -25,68 +8,88 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import main.java.it.unisa.ErasmusTracking.bean.Account;
+import main.java.it.unisa.ErasmusTracking.model.dao.IAccountDao;
+import main.java.it.unisa.ErasmusTracking.model.jpa.AccountManager;
+
+
+
+
 @WebServlet("/AddAccount")
 public class AddAccount extends HttpServlet {
-    private static final long serialVersionUID = 1L;
+  private static final long serialVersionUID = 1L;
 
-    static boolean isDataSource = true;
-    static String db = "erasmusTracking";
-    static String username = "root";
-    static String password = "root";
+  static boolean isDataSource = true;
+  static String db = "erasmusTracking";
+  static String username = "root";
+  static String password = "root";
 
-    static IAccountDao manager = new AccountManager(db, username, password);
+  static IAccountDao manager = new AccountManager(db, username, password);
 
-    public AddAccount() {
-        super();
+  public AddAccount() {
+    super();
+  }
+
+  /**
+   *doGet.
+   * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+   *
+   * @param request
+   *
+   * @param response
+   *
+   * @throws ServletException
+   *
+   * @throws IOException
+   *
+   */
+  protected void doGet(HttpServletRequest request, HttpServletResponse response)
+      throws ServletException, IOException {
+    doPost(request,response);
+  }
+
+  /**
+   * doPost.
+   *
+   * @param request
+   *
+   * @param response
+   *
+   * @throws ServletException
+   *
+   * @throws IOException
+   *
+   */
+  protected void doPost(HttpServletRequest request, HttpServletResponse response)
+      throws ServletException, IOException {
+
+    String nome = request.getParameter("nome");
+    String cognome = request.getParameter("cognome");
+    String email = request.getParameter("email");
+    String password = request.getParameter("password");
+    String ruolo = request.getParameter("ruolo");
+
+    Account account = new Account();
+
+    account.setNome(nome);
+    account.setCognome(cognome);
+    account.setEmail(email);
+    account.setPassword(password);
+    account.setRuolo(ruolo);
+
+    try {
+      manager.doSave(account);
+
+    } catch (NullPointerException e) {
+      e.printStackTrace();
     }
 
-    /**
-     *
-     * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-     * @param request
-     * @param response
-     * @throws ServletException
-     * @throws IOException
-     */
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        doPost(request,response);
-    }
 
-    /**
-     *
-     * @param request
-     * @param response
-     * @throws ServletException
-     * @throws IOException
-     */
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    //DA MODIFICARE NON APPENA CI SONO LE JSP
+    RequestDispatcher dispositivo =
+        getServletContext().getRequestDispatcher("/AccountServlet?action=doRetrieveAll");
+    dispositivo.forward(request, response);
 
-        String nome = request.getParameter("nome");
-        String cognome = request.getParameter("cognome");
-        String email = request.getParameter("email");
-        String password = request.getParameter("password");
-        String ruolo = request.getParameter("ruolo");
-
-        Account account = new Account();
-
-        account.setNome(nome);
-        account.setCognome(cognome);
-        account.setEmail(email);
-        account.setPassword(password);
-        account.setRuolo(ruolo);
-
-        try {
-            manager.doSave(account);
-
-        } catch(NullPointerException e){
-            e.printStackTrace();
-        }
-
-
-        //DA MODIFICARE NON APPENA CI SONO LE JSP
-        RequestDispatcher dispositivo = getServletContext().getRequestDispatcher("/AccountServlet?action=doRetrieveAll");
-        dispositivo.forward(request, response);
-
-    }
+  }
 
 }
--- src/main/java/it/unisa/ErasmusTracking/model/jpa/AccountManager.java
+++ src/main/java/it/unisa/ErasmusTracking/model/jpa/AccountManager.java
@@ -1,313 +1,331 @@
 package main.java.it.unisa.ErasmusTracking.model.jpa;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 import main.java.it.unisa.ErasmusTracking.bean.Account;
-import main.java.it.unisa.ErasmusTracking.bean.Coordinatore;
-import main.java.it.unisa.ErasmusTracking.bean.LearningAgreement;
-import main.java.it.unisa.ErasmusTracking.bean.Studente;
 import main.java.it.unisa.ErasmusTracking.model.dao.IAccountDao;
 import main.java.it.unisa.ErasmusTracking.util.DriverManagerConnectionPool;
 
+public class AccountManager implements IAccountDao {
+  private static final String TAB_NAME = "account"; //Nome tabella nel DB
 
-import java.sql.*;
-import java.util.ArrayList;
-import java.util.List;
+  public String db;
+  public String username;
+  public String password;
 
-public class AccountManager implements IAccountDao
-{
-    private static final String TAB_NAME = "account"; //Nome tabella nel DB
+  /**
+   *Costruttore.
+   */
+  public AccountManager(String db, String username, String password) {
+    this.db = db;
+    this.username = username;
+    this.password = password;
+  }
 
-    public String db;
-    public String username;
-    public String password;
-
-    public AccountManager(String db, String username, String password)
-    {
-        this.db = db;
-        this.username = username;
-        this.password = password;
-    }
-
-    //Genera query INSERT per salvare un nuovo elemento all'interno del DB
-    public synchronized void doSave(Object object){
-        Account account = (Account) object;
-        Connection connection = null;
-        PreparedStatement preparedStatement = null;
+  /**Genera query INSERT per salvare un nuovo elemento all'interno del DB.
+   */
+  public synchronized void doSave(Object object) {
+    Account account = (Account) object;
+    Connection connection = null;
+    PreparedStatement preparedStatement = null;
 
 
-        String insertSQL = "INSERT INTO " + AccountManager.TAB_NAME + "(nome, cognome, e_mail, password, ruolo) VALUES (?, ?, ?, ?, ?)";
+    String insertSql = "INSERT INTO " + AccountManager.TAB_NAME
+        +
+        "(nome, cognome, e_mail, password, ruolo) VALUES(?, ?, ?, ?, ?)";
 
+    try {
+      connection = DriverManagerConnectionPool.getConnection(db, username, password);
+      preparedStatement = connection.prepareStatement(insertSql);
+      preparedStatement.setString(1, account.getNome());
+      preparedStatement.setString(2, account.getCognome());
+      preparedStatement.setString(3, account.getEmail());
+      preparedStatement.setString(4, account.getPassword());
+      preparedStatement.setString(5, account.getRuolo());
+
+
+
+
+      preparedStatement.executeUpdate();
+
+      //connection.commit();
+    } catch (SQLException e) {
+      e.printStackTrace();
+    }  finally {
+      try {
+        if (preparedStatement != null) {
+          preparedStatement.close();
+        }
+      } catch (SQLException e) {
+        e.printStackTrace();
+      } finally {
         try {
-            connection = DriverManagerConnectionPool.getConnection(db, username, password);
-            preparedStatement = connection.prepareStatement(insertSQL);
-            preparedStatement.setString(1, account.getNome());
-            preparedStatement.setString(2, account.getCognome());
-            preparedStatement.setString(3, account.getEmail());
-            preparedStatement.setString(4, account.getPassword());
-            preparedStatement.setString(5, account.getRuolo());
-
-
-
-
-            preparedStatement.executeUpdate();
-
-            //connection.commit();
-        } catch(SQLException e){
-            e.printStackTrace();
-        }  finally {
-            try {
-                if (preparedStatement != null)
-                    preparedStatement.close();
-            }
-            catch(SQLException e){
-                e.printStackTrace();
-            }
-            finally {
-                try {
-                    DriverManagerConnectionPool.releaseConnection(connection);
-                } catch (SQLException e) {
-                    e.printStackTrace();
-                }
-            }
-        }
-    }
-
-    public synchronized boolean doDelete(int id)
-    {
-        Connection connection = null;
-        PreparedStatement preparedStatement = null;
-
-        int result = 0;
-
-        String deleteSQL = "DELETE FROM " + AccountManager.TAB_NAME + " WHERE id = ?";
-
-        try
-        {
-            connection = DriverManagerConnectionPool.getConnection(db,username,password);
-            preparedStatement = connection.prepareStatement(deleteSQL);
-            preparedStatement.setInt(1,id);
-
-            result = preparedStatement.executeUpdate();
-            connection.commit();
-
-        }
-        catch(SQLException e)
-        {
-            e.printStackTrace();
-        }
-
-        finally
-        {
-            try
-            {
-                if(preparedStatement!=null)
-                    preparedStatement.close();
-            }
-            catch(SQLException e)
-            {
-                e.printStackTrace();
-            }
-            finally
-            {
-                try
-                {
-                    DriverManagerConnectionPool.releaseConnection(connection);
-                }
-                catch(SQLException e)
-                {
-                    e.printStackTrace();
-                }
-            }
-        }
-
-        return (result!=0);
-    }
-
-
-    //Genera query SELECT per ricevere i dati in base a quella determinata key
-
-    public synchronized Account doRetrieveById(int id) {
-
-        Connection connection = null;
-        PreparedStatement preparedStatement = null;
-
-        Account bean = new Account();
-
-        String selectSQL = "SELECT * FROM " + AccountManager.TAB_NAME + " WHERE id_account = ?";
-
-        try {
-            connection = DriverManagerConnectionPool.getConnection(db, username, password);
-            preparedStatement = connection.prepareStatement(selectSQL);
-            preparedStatement.setInt(1, id);
-
-            ResultSet rs = preparedStatement.executeQuery();
-
-            while (rs.next()) {
-                bean.setId(rs.getInt("id_account"));
-                bean.setNome(rs.getString("nome"));
-                bean.setCognome(rs.getString("cognome"));
-                bean.setEmail(rs.getString("e_mail"));
-                bean.setPassword(rs.getString("password"));
-                bean.setRuolo(rs.getString("ruolo"));
-            }
-
-        } catch(SQLException e){
-            e.printStackTrace();
-        }  finally {
-            try {
-                if (preparedStatement != null)
-                    preparedStatement.close();
-            } catch (SQLException e) {
-                e.printStackTrace();
-            } finally {
-                try {
-                    DriverManagerConnectionPool.releaseConnection(connection);
-                } catch (SQLException e) {
-                    e.printStackTrace();
-                }
-            }
-        }
-        return bean;
-    }
-
-    public synchronized List<Account> doRetrieveAll() {
-
-        Connection connection = null;
-        PreparedStatement preparedStatement = null;
-
-        List<Account> account = new ArrayList<Account>();
-
-        String selectSQL = "SELECT * FROM " + AccountManager.TAB_NAME;
-        try {
-            connection = DriverManagerConnectionPool.getConnection(db, username, password);
-            preparedStatement = connection.prepareStatement(selectSQL);
-
-            ResultSet rs = preparedStatement.executeQuery();
-
-            while (rs.next())
-            {
-                Account bean = new Account();
-
-                bean.setId(rs.getInt("id_account"));
-                bean.setNome(rs.getString("nome"));
-                bean.setCognome(rs.getString("cognome"));
-                bean.setEmail(rs.getString("e_mail"));
-                bean.setPassword(rs.getString("password"));
-                bean.setRuolo(rs.getString("ruolo"));
-
-                account.add(bean);
-            }
-
-        } catch(SQLException e){
-            e.printStackTrace();
-        }  finally {
-            try {
-                if (preparedStatement != null)
-                    preparedStatement.close();
-            } catch (SQLException e) {
-                e.printStackTrace();
-            } finally {
-                try {
-                    DriverManagerConnectionPool.releaseConnection(connection);
-                } catch (SQLException e) {
-                    e.printStackTrace();
-                }
-            }
-        }
-        return account;
-
-    }
-
-
-    public synchronized Account doRetrieveByEmail(String email) {
-
-        Connection connection = null;
-        PreparedStatement preparedStatement = null;
-        Account bean = new Account();
-
-        String selectSQL = "SELECT account.id_account, account.nome, account.cognome, account.e_mail, account.password, account.ruolo FROM " +
-                AccountManager.TAB_NAME + " WHERE account.e_mail = ?";
-        try {
-            connection = DriverManagerConnectionPool.getConnection(db, username, password);
-            preparedStatement = connection.prepareStatement(selectSQL);
-            preparedStatement.setString(1, email);
-            ResultSet rs = preparedStatement.executeQuery();
-
-            while (rs.next()) {
-                bean.setId(rs.getInt("id_account"));
-                bean.setNome(rs.getString("nome"));
-                bean.setCognome(rs.getString("cognome"));
-                bean.setEmail(rs.getString("e_mail"));
-                bean.setPassword(rs.getString("password"));
-                bean.setRuolo(rs.getString("ruolo"));
-            }
-
-        } catch(SQLException e){
-            e.printStackTrace();
-        } finally{
-            try {
-                if (preparedStatement != null)
-                    preparedStatement.close();
-            } catch (SQLException e) {
-                e.printStackTrace();
-            } finally {
-                try {
-                    DriverManagerConnectionPool.releaseConnection(connection);
-                } catch (SQLException e) {
-                    e.printStackTrace();
-                }
-            }
-        }
-        return bean;
-
-    }
-
-    public synchronized void doUpdate(Object object) {
-
-        Account account = (Account) object;
-        account = doRetrieveByEmail(account.getEmail());
-
-        Connection connection = null;
-        PreparedStatement preparedStatement = null;
-
-        String insertSQL = "UPDATE " + AccountManager.TAB_NAME + " " +
-                "SET nome = ?, cognome = ?, password = ? " +
-                "WHERE id_account = ? ;";
-
-
-        try {
-            connection = DriverManagerConnectionPool.getConnection(db, username, password);
-            preparedStatement = connection.prepareStatement(insertSQL);
-
-            // TAB LEARNING AGREEMENT
-
-            preparedStatement.setString(1, account.getNome());
-            preparedStatement.setString(2, account.getCognome());
-            preparedStatement.setString(3, account.getPassword()); //
-            preparedStatement.setInt(4, account.getId());
-
-
-            //
-
-
-            preparedStatement.executeUpdate();
-
-            //  connection.commit();
+          DriverManagerConnectionPool.releaseConnection(connection);
         } catch (SQLException e) {
-            e.printStackTrace();
-        } finally {
-            try {
-                if (preparedStatement != null)
-                    preparedStatement.close();
-            } catch (SQLException e) {
-                e.printStackTrace();
-            } finally {
-                try {
-                    DriverManagerConnectionPool.releaseConnection(connection);
-                } catch (SQLException e) {
-                    e.printStackTrace();
-                }
-            }
+          e.printStackTrace();
         }
+      }
     }
+  }
+
+  /**
+   *doDelete.
+   */
+
+  public synchronized boolean doDelete(int id) {
+    Connection connection = null;
+    PreparedStatement preparedStatement = null;
+
+    int result = 0;
+
+    String deleteSql = "DELETE FROM " + AccountManager.TAB_NAME + " WHERE id = ?";
+
+    try {
+      connection = DriverManagerConnectionPool.getConnection(db,username,password);
+      preparedStatement = connection.prepareStatement(deleteSql);
+      preparedStatement.setInt(1,id);
+
+      result = preparedStatement.executeUpdate();
+      connection.commit();
+
+    } catch (SQLException e) {
+      e.printStackTrace();
+    } finally {
+      try {
+        if (preparedStatement != null) {
+          preparedStatement.close();
+        }
+      } catch (SQLException e) {
+        e.printStackTrace();
+      } finally {
+        try {
+          DriverManagerConnectionPool.releaseConnection(connection);
+        } catch (SQLException e) {
+          e.printStackTrace();
+        }
+      }
+    }
+
+    return (result != 0);
+  }
+
+
+  /**Genera query SELECT per ricevere i dati in base a quella determinata key.
+   */
+  public synchronized Account doRetrieveById(int id) {
+
+    Connection connection = null;
+    PreparedStatement preparedStatement = null;
+
+    Account bean = new Account();
+
+    String selectSql = "SELECT * FROM " + AccountManager.TAB_NAME + " WHERE id_account = ?";
+
+    try {
+      connection = DriverManagerConnectionPool.getConnection(db, username, password);
+      preparedStatement = connection.prepareStatement(selectSql);
+      preparedStatement.setInt(1, id);
+
+      ResultSet rs = preparedStatement.executeQuery();
+
+      while (rs.next()) {
+        bean.setId(rs.getInt("id_account"));
+        bean.setNome(rs.getString("nome"));
+        bean.setCognome(rs.getString("cognome"));
+        bean.setEmail(rs.getString("e_mail"));
+        bean.setPassword(rs.getString("password"));
+        bean.setRuolo(rs.getString("ruolo"));
+      }
+
+    } catch (SQLException e) {
+      e.printStackTrace();
+    }  finally {
+      try {
+        if (preparedStatement != null) {
+          preparedStatement.close();
+        }
+      } catch (SQLException e) {
+        e.printStackTrace();
+      } finally {
+        try {
+          DriverManagerConnectionPool.releaseConnection(connection);
+        } catch (SQLException e) {
+          e.printStackTrace();
+        }
+      }
+    }
+    return bean;
+  }
+
+  /**
+   *doRetriveAll.
+   */
+
+  public synchronized List<Account> doRetrieveAll() {
+
+    Connection connection = null;
+    PreparedStatement preparedStatement = null;
+
+    List<Account> account = new ArrayList<Account>();
+
+    String selectSql = "SELECT * FROM " + AccountManager.TAB_NAME;
+    try {
+      connection = DriverManagerConnectionPool.getConnection(db, username, password);
+      preparedStatement = connection.prepareStatement(selectSql);
+
+      ResultSet rs = preparedStatement.executeQuery();
+
+      while (rs.next()) {
+        Account bean = new Account();
+
+        bean.setId(rs.getInt("id_account"));
+        bean.setNome(rs.getString("nome"));
+        bean.setCognome(rs.getString("cognome"));
+        bean.setEmail(rs.getString("e_mail"));
+        bean.setPassword(rs.getString("password"));
+        bean.setRuolo(rs.getString("ruolo"));
+
+        account.add(bean);
+      }
+
+    } catch (SQLException e) {
+      e.printStackTrace();
+    }  finally {
+      try {
+        if (preparedStatement != null) {
+          preparedStatement.close();
+        }
+      } catch (SQLException e) {
+        e.printStackTrace();
+      } finally {
+        try {
+          DriverManagerConnectionPool.releaseConnection(connection);
+        } catch (SQLException e) {
+          e.printStackTrace();
+        }
+      }
+    }
+    return account;
+
+  }
+
+  /**
+   *doRetriveByEmail.
+   */
+
+  public synchronized Account doRetrieveByEmail(String email) {
+
+    Connection connection = null;
+    PreparedStatement preparedStatement = null;
+    Account bean = new Account();
+
+    String selectSql = "SELECT "
+            +
+            "account.id_account, account.nome, "
+            +
+            "account.cognome, account.e_mail, "
+            +
+            "account.password, account.ruolo "
+            +
+            "FROM "
+            +
+            AccountManager.TAB_NAME
+            +
+            " WHERE account.e_mail = ?";
+    try {
+      connection = DriverManagerConnectionPool.getConnection(db, username, password);
+      preparedStatement = connection.prepareStatement(selectSql);
+      preparedStatement.setString(1, email);
+      ResultSet rs = preparedStatement.executeQuery();
+
+      while (rs.next()) {
+        bean.setId(rs.getInt("id_account"));
+        bean.setNome(rs.getString("nome"));
+        bean.setCognome(rs.getString("cognome"));
+        bean.setEmail(rs.getString("e_mail"));
+        bean.setPassword(rs.getString("password"));
+        bean.setRuolo(rs.getString("ruolo"));
+      }
+
+    } catch (SQLException e) {
+      e.printStackTrace();
+    } finally {
+      try {
+        if (preparedStatement != null) {
+          preparedStatement.close();
+        }
+      } catch (SQLException e) {
+        e.printStackTrace();
+      } finally {
+        try {
+          DriverManagerConnectionPool.releaseConnection(connection);
+        } catch (SQLException e) {
+          e.printStackTrace();
+        }
+      }
+    }
+    return bean;
+
+  }
+
+  /**
+   *doUpdate.
+   */
+
+  public synchronized void doUpdate(Object object) {
+
+    Account account = (Account) object;
+    account = doRetrieveByEmail(account.getEmail());
+
+    Connection connection = null;
+    PreparedStatement preparedStatement = null;
+
+    String insertSql = "UPDATE " + AccountManager.TAB_NAME + " "
+        +
+        "SET nome = ?, cognome = ?, password = ? "
+        +
+        "WHERE id_account = ? ;";
+
+
+    try {
+      connection = DriverManagerConnectionPool.getConnection(db, username, password);
+      preparedStatement = connection.prepareStatement(insertSql);
+
+      // TAB LEARNING AGREEMENT
+
+      preparedStatement.setString(1, account.getNome());
+      preparedStatement.setString(2, account.getCognome());
+      preparedStatement.setString(3, account.getPassword()); //
+      preparedStatement.setInt(4, account.getId());
+
+
+      //
+
+
+      preparedStatement.executeUpdate();
+
+      //  connection.commit();
+    } catch (SQLException e) {
+      e.printStackTrace();
+    } finally {
+      try {
+        if (preparedStatement != null) {
+          preparedStatement.close();
+        }
+      } catch (SQLException e) {
+        e.printStackTrace();
+      } finally {
+        try {
+          DriverManagerConnectionPool.releaseConnection(connection);
+        } catch (SQLException e) {
+          e.printStackTrace();
+        }
+      }
+    }
+  }
 
 }
