67fe6c73e7bbd690dc8e11f00e6fe02749d77c53
Alexert97 <38686550+Alexert97@users.noreply.github.com>
2019-01-12 13:45:22 +0100

    Style: AddSendingistitute AddTicket

--- src/main/java/it/unisa/ErasmusTracking/controller/AddTicket.java
+++ src/main/java/it/unisa/ErasmusTracking/controller/AddTicket.java
@@ -1,20 +1,9 @@
 package main.java.it.unisa.ErasmusTracking.controller;
 
-
-
-import main.java.it.unisa.ErasmusTracking.bean.*;
-import main.java.it.unisa.ErasmusTracking.model.dao.*;
-import main.java.it.unisa.ErasmusTracking.model.jpa.*;
-
 import java.io.IOException;
-import java.sql.SQLException;
-import java.text.DateFormat;
 import java.time.LocalDate;
-import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.Collection;
-import java.util.List;
-
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -24,85 +13,108 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+import main.java.it.unisa.ErasmusTracking.bean.Account;
+import main.java.it.unisa.ErasmusTracking.bean.Studente;
+import main.java.it.unisa.ErasmusTracking.bean.Ticket;
+import main.java.it.unisa.ErasmusTracking.model.dao.IStudenteDao;
+import main.java.it.unisa.ErasmusTracking.model.dao.ITicketDao;
+import main.java.it.unisa.ErasmusTracking.model.jpa.StudenteManager;
+import main.java.it.unisa.ErasmusTracking.model.jpa.TicketManager;
+
 @WebServlet("/AddTicket")
 public class AddTicket extends HttpServlet {
-    private static final long serialVersionUID = 1L;
+  private static final long serialVersionUID = 1L;
 
-    static boolean isDataSource = true;
-    static String db = "erasmusTracking";
-    static String username = "root";
-    static String password = "root";
+  static boolean isDataSource = true;
+  static String db = "erasmusTracking";
+  static String username = "root";
+  static String password = "root";
 
-    static ITicketDao manager = new TicketManager(db, username, password);
-    static IStudenteDao studenteManager = new StudenteManager(db, username, password);
+  static ITicketDao manager = new TicketManager(db, username, password);
+  static IStudenteDao studenteManager = new StudenteManager(db, username, password);
 
-    public AddTicket() {
-        super();
+  public AddTicket() {
+    super();
+  }
+
+  /**
+   *doGet.
+   *
+   * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+   *
+   * @param request
+   *
+   * @param response
+   *
+   * @throws ServletException
+   *
+   * @throws IOException
+   *
+   */
+  protected void doGet(HttpServletRequest request, HttpServletResponse response)
+      throws ServletException, IOException {
+    doPost(request,response);
+  }
+
+  /**
+   * doPost.
+   *
+   * @param request
+   *
+   * @param response
+   *
+   * @throws ServletException
+   *
+   * @throws IOException
+   *
+   */
+  protected void doPost(HttpServletRequest request, HttpServletResponse response)
+      throws ServletException, IOException {
+
+    HttpSession session = request.getSession();
+
+    String oggetto = request.getParameter("oggetto");
+    Account account = (Account) session.getAttribute("utente");
+
+    int mittente;
+    int destinatario;
+    if (account.getRuolo().equalsIgnoreCase("studente")) {
+      mittente = account.getId();
+      Studente studente = (Studente) studenteManager.doRetrieveById(mittente);
+      destinatario = studente.getIdCoordinatore();
+    } else {
+      destinatario = account.getId();
+      //parametro inserito da profile.jsp <input type="hidden">
+      mittente = Integer.parseInt(request.getParameter("studente"));
     }
 
-    /**
-     *
-     * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-     * @param request
-     * @param response
-     * @throws ServletException
-     * @throws IOException
-     */
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        doPost(request,response);
+    boolean stato = true;
+
+    LocalDate date = LocalDate.now();
+    DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+    String dateFormatted = date.format(formatter); //data in dd/mm/yyyy
+    Ticket ticket = new Ticket();
+
+    ticket.setObject(oggetto);
+    ticket.setDatacreazione(dateFormatted);
+    ticket.setMittente(mittente);
+    ticket.setDestinatario(destinatario);
+
+
+    try {
+      manager.doSave(ticket);
+
+    } catch (NullPointerException e) {
+      e.printStackTrace();
     }
 
-    /**
-     *
-     * @param request
-     * @param response
-     * @throws ServletException
-     * @throws IOException
-     */
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    Collection<Ticket> tickets = manager.doRetrieveByIdStudente(mittente);
 
-        HttpSession session = request.getSession();
+    //DA MODIFICARE NON APPENA CI SONO LE JSP
+    RequestDispatcher dispositivo =
+        getServletContext().getRequestDispatcher("/TicketServlet?action=doRetrieveByIdAccount");
+    dispositivo.forward(request, response);
 
-        String oggetto= request.getParameter("oggetto");
-        Account account = (Account) session.getAttribute("utente");
-
-        int mittente;
-        int destinatario;
-        if (account.getRuolo().equalsIgnoreCase("studente")) {
-            mittente = account.getId();
-            Studente studente = (Studente) studenteManager.doRetrieveById(mittente);
-            destinatario = studente.getIdCoordinatore();
-        } else {
-            destinatario = account.getId();
-            mittente = Integer.parseInt(request.getParameter("studente")); //parametro inserito da profile.jsp <input type="hidden">
-        }
-
-        boolean stato = true;
-
-        LocalDate date = LocalDate.now();
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
-        String dateFormatted = date.format(formatter); //data in dd/mm/yyyy
-        Ticket ticket = new Ticket();
-
-        ticket.setObject(oggetto);
-        ticket.setDatacreazione(dateFormatted);
-        ticket.setMittente(mittente);
-        ticket.setDestinatario(destinatario);
-
-
-        try {
-            manager.doSave(ticket);
-
-        } catch(NullPointerException e){
-            e.printStackTrace();
-        }
-
-        Collection<Ticket> tickets = manager.doRetrieveByIdStudente(mittente);
-
-        //DA MODIFICARE NON APPENA CI SONO LE JSP
-        RequestDispatcher dispositivo = getServletContext().getRequestDispatcher("/TicketServlet?action=doRetrieveByIdAccount");
-        dispositivo.forward(request, response);
-
-    }
+  }
 
 }
