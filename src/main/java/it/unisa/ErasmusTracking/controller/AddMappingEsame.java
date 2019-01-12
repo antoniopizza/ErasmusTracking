30e00b4bbd95c8666d865c6b60c3e8af7a48a347
Alexert97 <38686550+Alexert97@users.noreply.github.com>
2019-01-12 16:14:18 +0100

    Style:AddMappingEsame

--- src/main/java/it/unisa/ErasmusTracking/controller/AddMappingEsame.java
+++ src/main/java/it/unisa/ErasmusTracking/controller/AddMappingEsame.java
@@ -1,56 +1,63 @@
 package main.java.it.unisa.ErasmusTracking.controller;
 
-import main.java.it.unisa.ErasmusTracking.bean.LearningAgreement;
-import main.java.it.unisa.ErasmusTracking.bean.MappingEsame;
-import main.java.it.unisa.ErasmusTracking.model.dao.IMappingEsameDao;
-import main.java.it.unisa.ErasmusTracking.model.jpa.MappingEsameManager;
-
+import java.io.IOException;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
+
+import main.java.it.unisa.ErasmusTracking.bean.LearningAgreement;
+import main.java.it.unisa.ErasmusTracking.bean.MappingEsame;
+import main.java.it.unisa.ErasmusTracking.model.dao.IMappingEsameDao;
+import main.java.it.unisa.ErasmusTracking.model.jpa.MappingEsameManager;
+
+
 
 @WebServlet("/AddMappingEsame")
 public class AddMappingEsame extends HttpServlet {
-    private static final long serialVersionUID = 1L;
+  private static final long serialVersionUID = 1L;
 
-    static boolean isDataSource = true;
-    static String db = "erasmusTracking";
-    static String username = "root";
-    static String password = "root";
+  static boolean isDataSource = true;
+  static String db = "erasmusTracking";
+  static String username = "root";
+  static String password = "root";
 
-    static IMappingEsameDao manager = new MappingEsameManager(db, username, password);
-
-
-    public AddMappingEsame() {
-        super();
-    }
+  static IMappingEsameDao manager = new MappingEsameManager(db, username, password);
 
 
-    /**
-     * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-     */
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        LearningAgreement learningAgreement = (LearningAgreement) request.getAttribute("learningAgreement");
-
-        MappingEsame mappingEsame = new MappingEsame();
-        mappingEsame.setLearningAgreement(learningAgreement.getId());
-
-        manager.doSave(mappingEsame);
-
-        RequestDispatcher dispositivo =
-                getServletContext().getRequestDispatcher("/learningAgreement.jsp");
-        dispositivo.forward(request, response);
-
-    }
+  public AddMappingEsame() {
+    super();
+  }
 
 
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        // TODO Auto-generated method stub
-        doGet(request, response);
-    }
+  /**
+   * doGet.
+   *
+   * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+   *
+   */
+  protected void doGet(HttpServletRequest request, HttpServletResponse response)
+      throws ServletException, IOException {
+    LearningAgreement learningAgreement =
+        (LearningAgreement) request.getAttribute("learningAgreement");
+    MappingEsame mappingEsame = new MappingEsame();
+    mappingEsame.setLearningAgreement(learningAgreement.getId());
+
+    manager.doSave(mappingEsame);
+
+    RequestDispatcher dispositivo =
+        getServletContext().getRequestDispatcher("/learningAgreement.jsp");
+    dispositivo.forward(request, response);
+
+  }
+
+
+  protected void doPost(HttpServletRequest request, HttpServletResponse response)
+      throws ServletException, IOException {
+    // TODO Auto-generated method stub
+    doGet(request, response);
+  }
 
 }
