c2c4af49634c20d5b891c85ab7f67e1e1febb478
Alexert97 <38686550+Alexert97@users.noreply.github.com>
2019-01-12 17:38:44 +0100

    Style: connection pull 1 e 2 Documenti Servlet

--- src/main/java/it/unisa/ErasmusTracking/controller/DocumentiServlet.java
+++ src/main/java/it/unisa/ErasmusTracking/controller/DocumentiServlet.java
@@ -1,16 +1,8 @@
 package main.java.it.unisa.ErasmusTracking.controller;
 
-import main.java.it.unisa.ErasmusTracking.bean.Account;
-import main.java.it.unisa.ErasmusTracking.bean.Documenti;
-import main.java.it.unisa.ErasmusTracking.model.dao.IDocumentoDao;
-import main.java.it.unisa.ErasmusTracking.model.jpa.DocumentiManager;
-
 import java.io.IOException;
 import java.io.InputStream;
-import java.sql.Blob;
-import java.sql.SQLException;
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.List;
 
 import javax.servlet.RequestDispatcher;
@@ -20,120 +12,139 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import main.java.it.unisa.ErasmusTracking.bean.Account;
+import main.java.it.unisa.ErasmusTracking.bean.Documenti;
+import main.java.it.unisa.ErasmusTracking.model.dao.IDocumentoDao;
+import main.java.it.unisa.ErasmusTracking.model.jpa.DocumentiManager;
+
+
 @WebServlet("/DocumentiServlet")
 public class DocumentiServlet extends HttpServlet {
-    private static final long serialVersionUID = 1L;
+  private static final long serialVersionUID = 1L;
 
-    static boolean isDataSource = true;
-    static String db = "erasmusTracking";
-    static String username = "root";
-    static String password = "root";
+  static boolean isDataSource = true;
+  static String db = "erasmusTracking";
+  static String username = "root";
+  static String password = "root";
 
-    static IDocumentoDao manager = new DocumentiManager(db, username, password);
-
-
-    public DocumentiServlet() {
-        super();
-    }
+  static IDocumentoDao manager = new DocumentiManager(db, username, password);
 
 
-
-    /**
-     * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-     */
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        //Riceve il parametro per capire quale azione effettuare
-        String action = request.getParameter("action");
-        //Riceve la pagina che ha aggiunto l'articolo al carrello per poterci tornare
-        String page = request.getParameter("page");
+  public DocumentiServlet() {
+    super();
+  }
 
 
-        System.out.println("Aggiunto in pagina: " + page);
+
+  /**
+   * doGet.
+   *
+   * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+   *
+   */
+
+  protected void doGet(HttpServletRequest request, HttpServletResponse response)
+      throws ServletException, IOException {
+    //Riceve il parametro per capire quale azione effettuare
+    String action = request.getParameter("action");
+    //Riceve la pagina che ha aggiunto l'articolo al carrello per poterci tornare
+    String page = request.getParameter("page");
 
 
-        try {
-            if (action != null) {
-                 if (action.equalsIgnoreCase("doRetrieveById")){
-                    int id = Integer.parseInt(request.getParameter("id"));
-
-                    Documenti documento =(Documenti) manager.doRetrieveById(id);
-                    request.removeAttribute("documento");
-                    request.setAttribute("documento", documento);
-
-                    //DA MODIFICARE NON APPENA CI SONO LE JSP
-                    RequestDispatcher dispositivo = getServletContext().getRequestDispatcher("/newCliente.jsp");
-                    dispositivo.forward(request, response);
-
-                } else if (action.equalsIgnoreCase("doRetrieveDocumentByUsernameStudent")){
-                    String username = request.getParameter("username");
-                    List<Documenti> documenti = manager.doRetrieveByUsernameStudent(username);
-                    request.removeAttribute("listaDocumenti");
-                    request.setAttribute("listaDocumenti", documenti);
-
-                    //DA MODIFICARE NON APPENA CI SONO LE JSP
-                    RequestDispatcher dispositivo = getServletContext().getRequestDispatcher("/newCliente.jsp");
-                    dispositivo.forward(request, response);
-
-                }  else if (action.equalsIgnoreCase("doRetrieveAll")){
-                    List<Documenti> documenti = (ArrayList<Documenti>) manager.doRetrieveAll();
-                    request.removeAttribute("listaDocumenti");
-                    request.setAttribute("listaDocumenti", documenti);
-
-                    //DA MODIFICARE NON APPENA CI SONO LE JSP
-                    RequestDispatcher dispositivo = getServletContext().getRequestDispatcher("/documenti.jsp");
-                    dispositivo.forward(request, response);
-
-                }  else if (action.equalsIgnoreCase("doRetrieveByIdAccount")){
-                     Account account = (Account) request.getSession().getAttribute("utente");
-
-                     List<Documenti> documenti = manager.doRetrieveByIdAccount(account.getId());
-                    request.removeAttribute("listaDocumenti");
-                    request.setAttribute("listaDocumenti", documenti);
-
-                    //DA MODIFICARE NON APPENA CI SONO LE JSP
-                    RequestDispatcher dispositivo = getServletContext().getRequestDispatcher("/documenti.jsp");
-                    dispositivo.forward(request, response);
-                } else if(action.equalsIgnoreCase("downloadById")) {
-                     int id = Integer.parseInt(request.getParameter("id"));
-                     Documenti documento = (Documenti) manager.doRetrieveById(id);
-                     request.removeAttribute("documento");
-                     request.setAttribute("documento", documento);
-
-                     System.out.println(Integer.toString(documento.getInputStream().available()));
-                     //Blob fileData = documento.getInputStream();
-
-                     response.setHeader("Content-Type", "application/pdf");
-
-                     response.setHeader("Content-Length", Integer.toString(documento.getInputStream().available()));
-
-                     response.setHeader("Content-Disposition", "inline; filename=\"" + documento.getNome() + ".pdf" + "\"");
-
-                     InputStream is = documento.getInputStream();
+    System.out.println("Aggiunto in pagina: " + page);
 
 
-                     byte[] bytes = new byte[1024];
-                     int bytesRead;
+    try {
+      if (action != null) {
+        if (action.equalsIgnoreCase("doRetrieveById")) {
+          int id = Integer.parseInt(request.getParameter("id"));
 
-                     while ((bytesRead = is.read(bytes)) != -1) {
-                         response.getOutputStream().write(bytes, 0, bytesRead);
-                     }
-                     is.close();
+          Documenti documento = (Documenti) manager.doRetrieveById(id);
+          request.removeAttribute("documento");
+          request.setAttribute("documento", documento);
 
-                        /*
-                     RequestDispatcher dispositivo = getServletContext().getRequestDispatcher("/documenti.jsp");
-                     dispositivo.forward(request, response);
-                      */
-                 }
+          //DA MODIFICARE NON APPENA CI SONO LE JSP
+          RequestDispatcher dispositivo =
+              getServletContext().getRequestDispatcher("/newCliente.jsp");
+          dispositivo.forward(request, response);
 
-            }
-        } catch (Exception e){
-            System.out.println("[DocumentiServlet.java] Errore: "+ e);
+        } else if (action.equalsIgnoreCase("doRetrieveDocumentByUsernameStudent")) {
+          String username = request.getParameter("username");
+          List<Documenti> documenti = manager.doRetrieveByUsernameStudent(username);
+          request.removeAttribute("listaDocumenti");
+          request.setAttribute("listaDocumenti", documenti);
+
+          //DA MODIFICARE NON APPENA CI SONO LE JSP
+          RequestDispatcher dispositivo =
+              getServletContext().getRequestDispatcher("/newCliente.jsp");
+          dispositivo.forward(request, response);
+
+        }  else if (action.equalsIgnoreCase("doRetrieveAll")) {
+          List<Documenti> documenti = (ArrayList<Documenti>) manager.doRetrieveAll();
+          request.removeAttribute("listaDocumenti");
+          request.setAttribute("listaDocumenti", documenti);
+
+          //DA MODIFICARE NON APPENA CI SONO LE JSP
+          RequestDispatcher dispositivo =
+              getServletContext().getRequestDispatcher("/documenti.jsp");
+          dispositivo.forward(request, response);
+
+        }  else if (action.equalsIgnoreCase("doRetrieveByIdAccount")) {
+          Account account = (Account) request.getSession().getAttribute("utente");
+
+          List<Documenti> documenti = manager.doRetrieveByIdAccount(account.getId());
+          request.removeAttribute("listaDocumenti");
+          request.setAttribute("listaDocumenti", documenti);
+
+          //DA MODIFICARE NON APPENA CI SONO LE JSP
+          RequestDispatcher dispositivo =
+              getServletContext().getRequestDispatcher("/documenti.jsp");
+          dispositivo.forward(request, response);
+        } else if (action.equalsIgnoreCase("downloadById")) {
+          int id = Integer.parseInt(request.getParameter("id"));
+          Documenti documento = (Documenti) manager.doRetrieveById(id);
+          request.removeAttribute("documento");
+          request.setAttribute("documento", documento);
+
+          System.out.println(Integer.toString(documento.getInputStream().available()));
+          //Blob fileData = documento.getInputStream();
+
+          response.setHeader("Content-Type", "application/pdf");
+
+          response.setHeader(
+              "Content-Length", Integer.toString(documento.getInputStream().available()));
+
+          response.setHeader(
+              "Content-Disposition", "inline; filename=\"" + documento.getNome() + ".pdf" + "\"");
+
+          InputStream is = documento.getInputStream();
+
+
+          byte[] bytes = new byte[1024];
+          int bytesRead;
+
+          while ((bytesRead = is.read(bytes)) != -1) {
+            response.getOutputStream().write(bytes, 0, bytesRead);
+          }
+          is.close();
+
+          /*
+       RequestDispatcher dispositivo = getServletContext().getRequestDispatcher("/documenti.jsp");
+       dispositivo.forward(request, response);
+        */
         }
+
+      }
+    } catch (Exception e) {
+      System.out.println("[DocumentiServlet.java] Errore: " + e);
     }
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        // TODO Auto-generated method stub
-        doGet(request, response);
-    }
+  }
+
+  protected void doPost(HttpServletRequest request, HttpServletResponse response)
+      throws ServletException, IOException {
+    // TODO Auto-generated method stub
+    doGet(request, response);
+  }
 
 
 
--- src/main/resource/DriverManagerConnectionPool.java
+++ src/main/resource/DriverManagerConnectionPool.java
@@ -8,68 +8,121 @@ import java.util.List;
 
 public class DriverManagerConnectionPool {
 
-    private static List<Connection> freeDbConnections;
+  private static List<Connection> freeDbConnections;
 
-    static {
-        freeDbConnections = new LinkedList<Connection>();
-        try {
-            Class.forName("com.mysql.jdbc.Driver");
-        } catch (ClassNotFoundException e) {
-            e.printStackTrace();
+  static {
+    freeDbConnections = new LinkedList<Connection>();
+    try {
+      Class.forName("com.mysql.jdbc.Driver");
+    } catch (ClassNotFoundException e) {
+      e.printStackTrace();
+    }
+  }
+
+  /**
+   * Crea una connessione con il database
+   * utilizzando i parametri db, username e password passati dall'esterno.
+   *
+   * @param db
+   *
+   * @param username
+   *
+   * @param password
+   *
+   * @return
+   *
+   * @throws SQLException
+   *
+   */
+  private static synchronized Connection createDbConnection(String db, String username,
+                                                            String password)
+      throws SQLException {
+    Connection newConnection = null;
+    String ip = "localhost";
+    String port = "3306";
+    db = "erasmusTracking";
+    username = "root";
+    password = "root";
+    try {
+      newConnection = DriverManager.getConnection(
+          "jdbc:mysql://"
+              +
+              ip
+              +
+              ":"
+              +
+              port
+              +
+              "/"
+              +
+              db
+              +
+              "?relaxAutoCommit=true", username, password);
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+    //newConnection.setAutoCommit(false);
+    //newConnection.toString();
+    return newConnection;
+  }
+
+
+  /**
+   * Restituisce una connessione col database se esistente, altrimenti la crea.
+   *
+   * @param db
+   *
+   * @param username
+   *
+   * @param password
+   *
+   * @return
+   *
+   * @throws SQLException
+   *
+   */
+  public static synchronized Connection getConnection(String db, String username,
+                                                      String password)
+      throws SQLException {
+    Connection connection;
+
+    /**
+     * la connessione è già presente
+     */
+    if (!freeDbConnections.isEmpty()) {
+      connection = (Connection) freeDbConnections.get(0);
+
+      freeDbConnections.remove(0);
+
+      try {
+        if (connection.isClosed()) {
+          connection = getConnection(db, username, password);
         }
+
+      } catch (SQLException e) {
+        connection.close();
+        connection = getConnection(db, username, password);
+      }
+    } else { //non esiste connessione, la creo
+
+      connection = createDbConnection(db, username, password);
     }
 
-    //Crea una connessione con il database utilizzando i parametri db, username e password passati dall'esterno
-    private static synchronized Connection createDBConnection(String db, String username, String password) throws SQLException {
-        Connection newConnection = null;
-        String ip = "localhost";
-        String port = "3306";
-        db = "erasmusTracking";
-        username = "root";
-        password = "root";
-        try {
-            newConnection = DriverManager.getConnection("jdbc:mysql://"+ip+":"+ port+"/"+db+"?relaxAutoCommit=true", username, password);
-        }
-        catch (Exception e) {
-            e.printStackTrace();
-        }
+    return connection;
+  }
 
-
-        //newConnection.setAutoCommit(false);
-        //newConnection.toString();
-        return newConnection;
+  /**
+   * Chiude la connessione col db.
+   *
+   * @param connection
+   *
+   * @throws SQLException
+   *
+   */
+  public static synchronized void releaseConnection(Connection connection)
+      throws SQLException {
+    if (connection != null) {
+      freeDbConnections.add(connection);
     }
-
-
-
-    //Restituisce una connessione col database se esistente, altrimenti la crea
-    public static synchronized Connection getConnection(String db, String username, String password) throws SQLException {
-        Connection connection;
-
-        if(!freeDbConnections.isEmpty()) {//la connessione è già presente
-            connection=(Connection) freeDbConnections.get(0);
-
-            freeDbConnections.remove(0);
-
-            try {
-                if (connection.isClosed())
-                    connection = getConnection(db, username, password);
-
-            } catch (SQLException e) {
-
-                connection.close();
-                connection = getConnection(db, username, password);
-            }
-        } else { //non esiste connessione, la creo
-
-            connection = createDBConnection(db, username, password);
-        }
-
-        return connection;
-    }
-
-    //Chiude la connessione col db
-    public static synchronized void releaseConnection(Connection connection) throws SQLException {
-        if(connection != null) freeDbConnections.add(connection);
-    }
+  }
 }
