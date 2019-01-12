1edd2ee34c5c631c8c8d21b729680b59ed4928a1
Alexert97 <38686550+Alexert97@users.noreply.github.com>
2019-01-12 16:46:07 +0100

    Style:Documenti DocumenitManager StudenteServlet

--- src/main/java/it/unisa/ErasmusTracking/controller/StudenteServlet.java
+++ src/main/java/it/unisa/ErasmusTracking/controller/StudenteServlet.java
@@ -1,17 +1,7 @@
 package main.java.it.unisa.ErasmusTracking.controller;
 
-import main.java.it.unisa.ErasmusTracking.bean.Account;
-import main.java.it.unisa.ErasmusTracking.bean.Documenti;
-import main.java.it.unisa.ErasmusTracking.bean.Studente;
-import main.java.it.unisa.ErasmusTracking.model.dao.IDocumentoDao;
-import main.java.it.unisa.ErasmusTracking.model.dao.IStudenteDao;
-import main.java.it.unisa.ErasmusTracking.model.jpa.DocumentiManager;
-import main.java.it.unisa.ErasmusTracking.model.jpa.StudenteManager;
-
 import java.io.IOException;
-import java.sql.SQLException;
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.List;
 
 import javax.servlet.RequestDispatcher;
@@ -21,116 +11,128 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import main.java.it.unisa.ErasmusTracking.bean.Account;
+import main.java.it.unisa.ErasmusTracking.bean.Studente;
+import main.java.it.unisa.ErasmusTracking.model.dao.IStudenteDao;
+import main.java.it.unisa.ErasmusTracking.model.jpa.StudenteManager;
+
+
+
 @WebServlet("/StudenteServlet")
 public class StudenteServlet extends HttpServlet {
-    private static final long serialVersionUID = 1L;
+  private static final long serialVersionUID = 1L;
 
-    static boolean isDataSource = true;
-    static String db = "erasmusTracking";
-    static String username = "root";
-    static String password = "root";
+  static boolean isDataSource = true;
+  static String db = "erasmusTracking";
+  static String username = "root";
+  static String password = "root";
 
-    static IStudenteDao manager = new StudenteManager(db, username, password);
-
-
-    public StudenteServlet() {
-        super();
-    }
+  static IStudenteDao manager = new StudenteManager(db, username, password);
 
 
-
-    /**
-     * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-     */
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        //Riceve il parametro per capire quale azione effettuare
-        String action = request.getParameter("action");
-        //Riceve la pagina che ha aggiunto l'articolo al carrello per poterci tornare
-        String page = request.getParameter("page");
+  public StudenteServlet() {
+    super();
+  }
 
 
-        System.out.println("Aggiunto in pagina: " + page);
+
+  /**
+   * doGet.
+   *
+   * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+   *
+   */
+  protected void doGet(HttpServletRequest request, HttpServletResponse response)
+      throws ServletException, IOException {
+    //Riceve il parametro per capire quale azione effettuare
+    String action = request.getParameter("action");
+    //Riceve la pagina che ha aggiunto l'articolo al carrello per poterci tornare
+    String page = request.getParameter("page");
 
 
-        try {
-            if (action != null) {
-                 if (action.equalsIgnoreCase("doRetrieveById")){
-                    int id = Integer.parseInt(request.getParameter("id"));
+    System.out.println("Aggiunto in pagina: " + page);
 
-                    Studente studente =(Studente) manager.doRetrieveById(id);
-                    request.removeAttribute("studente");
-                    request.setAttribute("studente", studente);
 
-                    //DA MODIFICARE NON APPENA CI SONO LE JSP
-                    RequestDispatcher dispositivo = getServletContext().getRequestDispatcher("/utente.jsp");
-                    dispositivo.forward(request, response);
+    try {
+      if (action != null) {
+        if (action.equalsIgnoreCase("doRetrieveById")){
+          int id = Integer.parseInt(request.getParameter("id"));
 
-                } else if (action.equalsIgnoreCase("doRetrieveByEmail")){
-                    String email = request.getParameter("email");
-                    Studente studente = (Studente) manager.doRetrieveByEmail(email);
-                    request.removeAttribute("studente");
-                    request.setAttribute("studente", studente);
+          Studente studente =(Studente) manager.doRetrieveById(id);
+          request.removeAttribute("studente");
+          request.setAttribute("studente", studente);
 
-                    //DA MODIFICARE NON APPENA CI SONO LE JSP
-                    RequestDispatcher dispositivo = getServletContext().getRequestDispatcher("/utente.jsp");
-                    dispositivo.forward(request, response);
+          //DA MODIFICARE NON APPENA CI SONO LE JSP
+          RequestDispatcher dispositivo = getServletContext().getRequestDispatcher("/utente.jsp");
+          dispositivo.forward(request, response);
 
-                }  else if (action.equalsIgnoreCase("doRetrieveAll")){
-                    List<Studente> studenti = (ArrayList<Studente>) manager.doRetrieveAll();
-                    request.removeAttribute("listaStudenti");
-                    request.setAttribute("listaStudenti", studenti);
+        } else if (action.equalsIgnoreCase("doRetrieveByEmail")) {
+          String email = request.getParameter("email");
+          Studente studente = (Studente) manager.doRetrieveByEmail(email);
+          request.removeAttribute("studente");
+          request.setAttribute("studente", studente);
 
-                    //DA MODIFICARE NON APPENA CI SONO LE JSP
-                    RequestDispatcher dispositivo = getServletContext().getRequestDispatcher("/utente.jsp");
-                    dispositivo.forward(request, response);
+          //DA MODIFICARE NON APPENA CI SONO LE JSP
+          RequestDispatcher dispositivo = getServletContext().getRequestDispatcher("/utente.jsp");
+          dispositivo.forward(request, response);
 
-                } else if (action.equalsIgnoreCase("doRetrieveByCoordinatore")) {
-                     Account coordinatore = (Account) request.getSession().getAttribute("utente");
-                     System.out.print(coordinatore.toString());
-                     List<Studente> studenti = (ArrayList<Studente>) manager.doRetrieveByCoordinatore(coordinatore.getId());
-                     //System.out.print(studenti.toString());
-                     request.removeAttribute("listaStudenti");
-                     request.setAttribute("listaStudenti", studenti);
+        }  else if (action.equalsIgnoreCase("doRetrieveAll")) {
+          List<Studente> studenti = (ArrayList<Studente>) manager.doRetrieveAll();
+          request.removeAttribute("listaStudenti");
+          request.setAttribute("listaStudenti", studenti);
 
-                     //DA MODIFICARE NON APPENA CI SONO LE JSP
-                     RequestDispatcher dispositivo = getServletContext().getRequestDispatcher("/utente.jsp");
-                     dispositivo.forward(request, response);
-                } else if (action.equalsIgnoreCase("doUpdateLearningAgreement")) {
-                    Studente studente = (Studente) request.getSession().getAttribute("utente");
-                    IStudenteDao manager = new StudenteManager(db, username, password);
-                    String nome = request.getParameter("nome");
-                    String cognome = request.getParameter("cognome");
-                    String email = request.getParameter("email");
-                    String data_di_nascita = request.getParameter("data_di_nascita");
-                    String luogo_di_nascita = request.getParameter("luogo_di_nascita");
-                    String codice_materia = request.getParameter("codice_materia");
-                    String telefono = request.getParameter("telefono");
-                    int anno_accademico = Integer.parseInt(request.getParameter("anno_accademico"));
+          //DA MODIFICARE NON APPENA CI SONO LE JSP
+          RequestDispatcher dispositivo = getServletContext().getRequestDispatcher("/utente.jsp");
+          dispositivo.forward(request, response);
 
-                    studente.setNome(nome);
-                    studente.setCognome(cognome);
-                    studente.setEmail(email);
-                    studente.setDataDiNascita(data_di_nascita);
-                    studente.setLuogoDiNascita(luogo_di_nascita);
-                    studente.setCodiceMateria(codice_materia);
-                    studente.setTelefono(telefono);
-                    studente.setAnnoAccademico(anno_accademico);
+        } else if (action.equalsIgnoreCase("doRetrieveByCoordinatore")) {
+          Account coordinatore = (Account) request.getSession().getAttribute("utente");
+          System.out.print(coordinatore.toString());
+          List<Studente> studenti =
+              (ArrayList<Studente>) manager.doRetrieveByCoordinatore(coordinatore.getId());
+          //System.out.print(studenti.toString());
+          request.removeAttribute("listaStudenti");
+          request.setAttribute("listaStudenti", studenti);
 
-                    manager.doUpdate(studente);
+          //DA MODIFICARE NON APPENA CI SONO LE JSP
+          RequestDispatcher dispositivo = getServletContext().getRequestDispatcher("/utente.jsp");
+          dispositivo.forward(request, response);
+        } else if (action.equalsIgnoreCase("doUpdateLearningAgreement")) {
+          Studente studente = (Studente) request.getSession().getAttribute("utente");
+          IStudenteDao manager = new StudenteManager(db, username, password);
+          String nome = request.getParameter("nome");
+          String cognome = request.getParameter("cognome");
+          String email = request.getParameter("email");
+          String data_di_nascita = request.getParameter("data_di_nascita");
+          String luogo_di_nascita = request.getParameter("luogo_di_nascita");
+          String codice_materia = request.getParameter("codice_materia");
+          String telefono = request.getParameter("telefono");
+          int anno_accademico = Integer.parseInt(request.getParameter("anno_accademico"));
 
-                    //DA MODIFICARE NON APPENA CI SONO LE JSP
-                    RequestDispatcher dispositivo = getServletContext().getRequestDispatcher("/learning-agreement.jsp");
-                    dispositivo.forward(request, response);
-                }
-            }
-        } catch (Exception e){
-            System.out.println("[CoordinatoreServlet.java] Errore: "+ e);
+          studente.setNome(nome);
+          studente.setCognome(cognome);
+          studente.setEmail(email);
+          studente.setDataDiNascita(data_di_nascita);
+          studente.setLuogoDiNascita(luogo_di_nascita);
+          studente.setCodiceMateria(codice_materia);
+          studente.setTelefono(telefono);
+          studente.setAnnoAccademico(anno_accademico);
+
+          manager.doUpdate(studente);
+
+          //DA MODIFICARE NON APPENA CI SONO LE JSP
+          RequestDispatcher dispositivo = getServletContext().getRequestDispatcher("/learning-agreement.jsp");
+          dispositivo.forward(request, response);
         }
+      }
+    } catch (Exception e){
+      System.out.println("[CoordinatoreServlet.java] Errore: "+ e);
     }
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+  }
+  protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
-        doGet(request, response);
-    }
+    doGet(request, response);
+  }
 
 
 
