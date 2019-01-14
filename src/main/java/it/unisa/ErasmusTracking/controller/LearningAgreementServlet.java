3ead72631e8fb95b7cd391f9c132951269ee0640
Alexert97 <38686550+Alexert97@users.noreply.github.com>
2019-01-14 12:48:29 +0100

    Style :  localita mapping esame localita servlet log in

--- src/main/java/it/unisa/ErasmusTracking/controller/LearningAgreementServlet.java
+++ src/main/java/it/unisa/ErasmusTracking/controller/LearningAgreementServlet.java
@@ -1,17 +1,6 @@
 package main.java.it.unisa.ErasmusTracking.controller;
 
-import main.java.it.unisa.ErasmusTracking.bean.Account;
-import main.java.it.unisa.ErasmusTracking.bean.Documenti;
-import main.java.it.unisa.ErasmusTracking.bean.LearningAgreement;
-import main.java.it.unisa.ErasmusTracking.model.dao.ILearningAgreementDao;
-import main.java.it.unisa.ErasmusTracking.model.jpa.DocumentiManager;
-import main.java.it.unisa.ErasmusTracking.model.jpa.LearningAgreementManager;
-
 import java.io.IOException;
-import java.sql.SQLException;
-import java.util.Collection;
-import java.util.List;
-
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -19,61 +8,76 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import main.java.it.unisa.ErasmusTracking.bean.Account;
+import main.java.it.unisa.ErasmusTracking.bean.LearningAgreement;
+import main.java.it.unisa.ErasmusTracking.model.dao.ILearningAgreementDao;
+import main.java.it.unisa.ErasmusTracking.model.jpa.LearningAgreementManager;
+
+
+
 @WebServlet("/LearningAgreementServlet")
 public class LearningAgreementServlet extends HttpServlet {
-    private static final long serialVersionUID = 1L;
+  private static final long serialVersionUID = 1L;
 
-    static boolean isDataSource = true;
-    static String db = "erasmusTracking";
-    static String username = "root";
-    static String password = "root";
+  static boolean isDataSource = true;
+  static String db = "erasmusTracking";
+  static String username = "root";
+  static String password = "root";
 
-    static ILearningAgreementDao manager = new LearningAgreementManager(db, username, password);
-
-
-    public LearningAgreementServlet() {
-        super();
-    }
+  static ILearningAgreementDao manager = new LearningAgreementManager(db, username, password);
 
 
-
-    /**
-     * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-     */
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        //Riceve il parametro per capire quale azione effettuare
-        String action = request.getParameter("action");
-        //Riceve la pagina che ha aggiunto l'articolo al carrello per poterci tornare
-        String page = request.getParameter("page");
+  public LearningAgreementServlet() {
+    super();
+  }
 
 
-        System.out.println("Aggiunto in pagina: " + page);
+
+  /**
+   * doGet.
+   *
+   * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+   *
+   */
+  protected void doGet(HttpServletRequest request, HttpServletResponse response)
+      throws ServletException, IOException {
+    //Riceve il parametro per capire quale azione effettuare
+    String action = request.getParameter("action");
+    //Riceve la pagina che ha aggiunto l'articolo al carrello per poterci tornare
+    String page = request.getParameter("page");
 
 
-        try {
-            if (action != null) {
-                if (action.equalsIgnoreCase("doRetrieveById")){
-                    int id = Integer.parseInt(request.getParameter("id"));
-                    //LearningAgreement learningAgreement = manager.doRetrieveById(id);
+    System.out.println("Aggiunto in pagina: " + page);
 
-                } else if (action.equalsIgnoreCase("doRetrieveByStudente")) {
-                    Account account = (Account) request.getSession().getAttribute("utente");
-                    LearningAgreement learningAgreement = manager.doRetrieveByStudente(account.getId());
-                    request.removeAttribute("learningAgreement");
-                    request.setAttribute("learningAgreement", learningAgreement);
 
-                    RequestDispatcher dispositivo = getServletContext().getRequestDispatcher("/MappingEsameServlet?action=doRetrieveByLearningAgreement");
-                    dispositivo.forward(request, response);
-                }
-            }
-        } catch (Exception e){
-            System.out.println("[Learning-agreementServlet.java] Errore: "+ e);
+    try {
+      if (action != null) {
+        if (action.equalsIgnoreCase("doRetrieveById")) {
+          int id = Integer.parseInt(request.getParameter("id"));
+          //LearningAgreement learningAgreement = manager.doRetrieveById(id);
+
+        } else if (action.equalsIgnoreCase("doRetrieveByStudente")) {
+          Account account = (Account) request.getSession().getAttribute("utente");
+          LearningAgreement learningAgreement = manager.doRetrieveByStudente(account.getId());
+          request.removeAttribute("learningAgreement");
+          request.setAttribute("learningAgreement", learningAgreement);
+
+          RequestDispatcher dispositivo =
+              getServletContext().getRequestDispatcher(
+                  "/MappingEsameServlet?action=doRetrieveByLearningAgreement");
+          dispositivo.forward(request, response);
         }
+      }
+    } catch (Exception e) {
+      System.out.println("[Learning-agreementServlet.java] Errore: " + e);
     }
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        // TODO Auto-generated method stub
-        doGet(request, response);
-    }
+  }
+
+  protected void doPost(HttpServletRequest request, HttpServletResponse response)
+      throws ServletException, IOException {
+    // TODO Auto-generated method stub
+    doGet(request, response);
+  }
 
 
 
--- src/main/java/it/unisa/ErasmusTracking/controller/LoginServlet.java
+++ src/main/java/it/unisa/ErasmusTracking/controller/LoginServlet.java
@@ -1,12 +1,6 @@
 package main.java.it.unisa.ErasmusTracking.controller;
 
-import main.java.it.unisa.ErasmusTracking.bean.Account;
-import main.java.it.unisa.ErasmusTracking.model.dao.IAccountDao;
-import main.java.it.unisa.ErasmusTracking.model.jpa.AccountManager;
-
 import java.io.IOException;
-import java.sql.SQLException;
-
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -14,76 +8,105 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+import main.java.it.unisa.ErasmusTracking.bean.Account;
+import main.java.it.unisa.ErasmusTracking.model.dao.IAccountDao;
+import main.java.it.unisa.ErasmusTracking.model.jpa.AccountManager;
+
+
+
 /**
- * Servlet implementation class AdminLogin
+ * Servlet implementation class AdminLogin.
+ *
  */
 @WebServlet("/LoginServlet")
 public class LoginServlet extends HttpServlet {
-    private static final long serialVersionUID = 1L;
+  private static final long serialVersionUID = 1L;
 
 
-    static String db = "erasmusTracking";
-    static String username = "root";
-    static String password = "root";
+  static String db = "erasmusTracking";
+  static String username = "root";
+  static String password = "root";
 
-    static IAccountDao model = new AccountManager(db,username,password);
+  static IAccountDao model = new AccountManager(db,username,password);
 
 
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public LoginServlet() {
-        super();
-        // TODO Auto-generated constructor stub
+  /**
+   * LoginServle.
+   *
+   * @see HttpServlet#HttpServlet()
+   *
+   */
+  public LoginServlet() {
+    super();
+    // TODO Auto-generated constructor stub
+  }
+
+  /**
+   * doGet.
+   *
+   *  @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+   *
+   */
+  protected void doGet(HttpServletRequest request, HttpServletResponse response)
+      throws ServletException, IOException {
+    System.out.println("Error. This servlet must be called with POST method.");
+  }
+
+  /**
+   * doPost.
+   *
+   * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+   *
+   */
+  protected void doPost(HttpServletRequest request, HttpServletResponse response)
+      throws ServletException, IOException {
+
+
+    String userForm = request.getParameter("username");
+    String passForm = request.getParameter("password");
+    System.out.println("Username: " + userForm + ", password: " + passForm);
+
+
+    Account account = new Account();
+    HttpSession session = request.getSession();
+    //Questa variabile a 0 serve per prevenire l'accesso non autorizzato alla pagina dopo il login
+    String ruolo = "";
+    //salvo la variabile nella session per leggerla dalle pagine autorizzate
+    session.setAttribute("ruolo", ruolo);
+
+    String linkReind = (String) session.getAttribute("link");
+    System.out.println(linkReind);
+    try {
+      request.removeAttribute("utente");
+      account = model.doRetrieveByEmail(userForm);
+    } catch (Exception e) {
+      System.out.println("[AdminLogin.java] Error: " + e);
     }
 
-    /**
-     * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-     */
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        System.out.println("Error. This servlet must be called with POST method.");
+    //username e password corrispondono
+    if (account.getEmail().equals(userForm) && account.getPassword().equals(passForm)) {
+      System.out.println(account.toString());
+      //salvo il nome dell'admin nella sessione
+      session.setAttribute("name", userForm);
+      //metto il bit di controllo admin a 1 per l'accesso autorizzato
+      ruolo = account.getRuolo();
+      session.removeAttribute("ruolo");
+      //inserisco il bit nella session per leggerlo dalle page autorizzate
+      session.setAttribute("ruolo", ruolo);
+      session.removeAttribute("utente");
+      session.setAttribute("utente", account);
+      //vado sulla pagina di errore login
+      response.sendRedirect(
+          request.getContextPath()
+              +
+              "/AccountServlet?action=doRetrieveById&id="
+              +
+              account.getId());
+    } else { //username o psw o entrambi errati
+      response.sendRedirect(
+          request.getContextPath() + "/loginFail.jsp"); //vado sulla pagina di errore login
     }
 
-    /**
-     * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-     */
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
-
-        String userForm=request.getParameter("username");
-        String passForm=request.getParameter("password");
-        System.out.println("Username: " + userForm + ", password: " + passForm);
-
-
-        Account account = new Account();
-        HttpSession session=request.getSession();
-        String ruolo = ""; //Questa variabile a 0 serve per prevenire l'accesso non autorizzato alla pagina dopo il login
-        session.setAttribute("ruolo", ruolo); //salvo la variabile nella session per leggerla dalle pagine autorizzate
-
-        String linkReind = (String) session.getAttribute("link");
-        System.out.println(linkReind);
-        try {
-            request.removeAttribute("utente");
-            account = model.doRetrieveByEmail(userForm);
-        } catch(Exception e) {
-            System.out.println("[AdminLogin.java] Error: " + e);
-        }
-
-        if(account.getEmail().equals(userForm) && account.getPassword().equals(passForm)) { //username e password corrispondono
-
-            System.out.println(account.toString());
-            session.setAttribute("name", userForm); //salvo il nome dell'admin nella sessione
-            ruolo = account.getRuolo(); //metto il bit di controllo admin a 1 per l'accesso autorizzato
-            session.removeAttribute("ruolo");
-            session.setAttribute("ruolo", ruolo); //inserisco il bit nella session per leggerlo dalle page autorizzate
-            session.removeAttribute("utente");
-            session.setAttribute("utente", account);
-            response.sendRedirect(request.getContextPath() + "/AccountServlet?action=doRetrieveById&id=" + account.getId()); //vado sulla pagina di errore login
-        }
-        else { //username o psw o entrambi errati
-            response.sendRedirect(request.getContextPath() + "/loginFail.jsp"); //vado sulla pagina di errore login
-        }
-
-    }
+  }
 
 }
