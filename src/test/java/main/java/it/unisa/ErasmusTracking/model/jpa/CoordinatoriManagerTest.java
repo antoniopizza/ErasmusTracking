6e18761e234245209c7891c2cf128f23018f3b27
Alexert97 <38686550+Alexert97@users.noreply.github.com>
2019-01-16 18:10:45 +0100

    Test:sistemato id account in studente

--- src/test/java/main/java/it/unisa/ErasmusTracking/model/jpa/CoordinatoriManagerTest.java
+++ src/test/java/main/java/it/unisa/ErasmusTracking/model/jpa/CoordinatoriManagerTest.java
@@ -1,19 +1,25 @@
 package main.java.it.unisa.ErasmusTracking.model.jpa;
 
 import main.java.it.unisa.ErasmusTracking.bean.Coordinatore;
+import main.java.it.unisa.ErasmusTracking.bean.Localita;
+import main.java.it.unisa.ErasmusTracking.bean.Studente;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.assertNotEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 
 
 class CoordinatoriManagerTest {
   private static CoordinatoriManager classUnderTest;
   private static Integer id;
   private static Coordinatore bean;
+  private static Studente studente;
+  private static Localita localita;
+
 
   @BeforeAll
   static void setUp() throws Exception {
@@ -59,11 +65,12 @@ class CoordinatoriManagerTest {
     System.out.println("delete");
 
     bean = new Coordinatore();
-    bean.setNome("Fabrizio");
-    bean.setCognome("Bellucci");
-    bean.setEmail("bellucci92@hotmail.it");
-    bean.setPassword("adenoidi");
-    bean.setRuolo("");
+    bean.setNome("Alessandro");
+    bean.setCognome("Rigido");
+    bean.setPassword("root");
+    bean.setEmail("a.rigido1@studenti.unisa.it");
+    bean.setRuolo("coordinatore");
+    bean.setSendingInstitute(1);
 
     classUnderTest.doSave(bean);
 
@@ -88,17 +95,100 @@ class CoordinatoriManagerTest {
 
   @Test
   void testDoRetrieveById() {
-  }
-
-  @Test
-  void testDoRetrieveByIdAccount() {
+    System.out.println("doRetrieveById");
+    bean = classUnderTest.doRetrieveById(id);
+    assertEquals(0, bean.getId());
   }
 
   @Test
   void testDoRetrieveAll() {
+    System.out.println("doRetrieveAll");
+
+    bean = new Coordinatore();
+    bean.setNome("Alessandro");
+    bean.setCognome("Rigido");
+    bean.setPassword("root");
+    bean.setEmail("a.rigido1@studenti.unisa.it");
+    bean.setRuolo("coordinatore");
+    bean.setSendingInstitute(1);
+
+    classUnderTest.doSave(bean);
+
+    List<Coordinatore> list = classUnderTest.doRetrieveAll();
+    assertNotEquals(0,list.size());
+
+    classUnderTest.doDelete(list.size()-1);
   }
 
-  @Test
+/*  @Test
   void testDoRetrieveByEmail() {
+    bean = new Coordinatore();
+    bean.setNome("Alessandro");
+    bean.setCognome("Rigido");
+    bean.setPassword("root");
+    bean.setEmail("a.rigido1@studenti.unisa.it");
+    bean.setRuolo("coordinatore");
+    bean.setSendingInstitute(1);
+
+    classUnderTest.doSave(bean);
+
+    Coordinatore coordinatore = classUnderTest.doRetrieveByEmail(bean.getEmail());
+
+    studente = new Studente();
+    studente.setAnnoAccademico(1);
+    studente.setDataDiNascita("12/!2/2018");
+    studente.setLuogoDiNascita("Caserta");
+    studente.setTelefono("30123818297");
+    studente.setCicloDiStudi("trianneale");
+    studente.setNazionalita("italia");
+    studente.setSesso("M");
+    studente.setEmail("aleoale@live.it");
+    studente.setCognome("Poldo");
+    studente.setMatricola("12313123123");
+    studente.setNome("alessandro");
+    studente.setPassword("root");
+    studente.setRuolo("studente");
+    studente.setCodiceMateria("asrdqa");
+    studente.setIdCoordinatore(coordinatore.getId());
+
+    bean.addStudente(studente);
+    coordinatore.addStudente(studente);
+
+
+    classUnderTest.doSave(bean);
+
+
+    bean.setId(coordinatore.getId());
+    assertEquals(bean,coordinatore);
+  }*/
+
+  @Test
+  void testDoUpdate() {
+    System.out.println("doUpdate");
+
+    bean = new Coordinatore();
+    bean.setNome("Alessandro");
+    bean.setCognome("Rigido");
+    bean.setPassword("root");
+    bean.setEmail("a.rigido1@studenti.unisa.it");
+    bean.setRuolo("coordinatore");
+    bean.setSendingInstitute(1);
+
+    classUnderTest.doSave(bean);
+
+    ArrayList<Coordinatore> list = (ArrayList<Coordinatore>) classUnderTest.doRetrieveAll();
+    Coordinatore bean = list.get(list.size() - 1);
+
+    bean.setCognome("RIjf");
+    bean.setNome("Paolo");
+
+    boolean ok = false;
+    try {
+      classUnderTest.doUpdate(bean);
+      ok = true;
+    } catch (Exception e) {
+      ok = false;
+    }
+    assertTrue(ok);
   }
 }
--- src/test/java/main/java/it/unisa/ErasmusTracking/model/jpa/StudenteManagerTest.java
+++ src/test/java/main/java/it/unisa/ErasmusTracking/model/jpa/StudenteManagerTest.java
@@ -1,14 +1,63 @@
 package main.java.it.unisa.ErasmusTracking.model.jpa;
 
 import main.java.it.unisa.ErasmusTracking.bean.Account;
+import main.java.it.unisa.ErasmusTracking.bean.Studente;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class StudenteManagerTest {
+    private static StudenteManager classUnderTest;
+    private static Integer id;
+    private static Studente bean;
+
+    @BeforeAll
+    static void setUp() throws Exception {
+        classUnderTest = new StudenteManager("erasmustracking","root","root");
+        id = 5;
+    }
 
     @Test
     void doSave() {
+        bean = new Studente();
+        bean.setAnnoAccademico(1);
+        bean.setDataDiNascita("12/12/2018");
+        bean.setLuogoDiNascita("Caserta");
+        bean.setTelefono("3012322297");
+        bean.setCicloDiStudi("1-triennale");
+        bean.setNazionalita("Italia");
+        bean.setSesso("M");
+        bean.setEmail("aleoale@live.it");
+        bean.setCognome("Poldo");
+        bean.setMatricola("0215456332");
+        bean.setNome("alessandro");
+        bean.setPassword("root");
+        bean.setRuolo("studente");
+        bean.setIdCoordinatore(1);
+        boolean ok = false;
+        try {
+            classUnderTest.doSave(bean);
+            ok = true;
+        } catch (Exception e) {
+            ok=false;
+            e.printStackTrace();
+        }
+
+        assertTrue(ok);
+
+        ArrayList<Studente> list = (ArrayList<Studente>) classUnderTest.doRetrieveAll();
+        assertNotEquals(0, list.size());
+
+        Integer id = 0;
+        for (Studente bean : list) {
+            id++;
+        }
+        System.out.println(id);
+        classUnderTest.doDelete(id);
+
 
 
     }
