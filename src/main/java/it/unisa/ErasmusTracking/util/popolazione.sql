1582230c1c244a04384f700c163328d37a600b7f
federipoli <federico.ripoli.96@gmail.com>
2019-01-16 14:43:31 +0100

    update: popolazione

--- src/main/java/it/unisa/ErasmusTracking/util/popolazione.sql
+++ src/main/java/it/unisa/ErasmusTracking/util/popolazione.sql
@@ -1,9 +1,13 @@
 use erasmusTracking;
 insert into account (nome, cognome, e_mail, password, ruolo) Values ('Silvio', 'Cresci', 'root@gmail.com','root','coordinatore');
 insert into account (nome, cognome, e_mail, password, ruolo) Values ('Pascale', 'Capitone', 'pascale@gmail.com','root','studente');
+insert into account (nome, cognome, e_mail, password, ruolo) Values ('Carlo', 'Di Domenico', 'chardido@gmail.com','pizza','amministratore');
 
-insert into coordinatore (sending_institute, account) Values (null, 1);
+insert into sendinginstitute (codice, dipartimento, indirizzo) Values ('15784', 'Unisa', 'Fisciano');
+
+insert into coordinatore (sending_institute, account) Values (1, 1);
 insert into studente (matricola, data_nascita, luogo_nascita, sesso, nazionalita, telefono, ciclo_studi, anno_accademico, account, coordinatore) Values ('1234','12/12/12','Napoli', 'M', 'Italia', '377822', '1-triennale', 2, 2, 1);
+insert into amministratore(account) Values (3);
 
 insert into location (citta, nazione, coordinatore, codice_erasmus) Values ('Roma', 'Polonia', 1, 054783);
 
