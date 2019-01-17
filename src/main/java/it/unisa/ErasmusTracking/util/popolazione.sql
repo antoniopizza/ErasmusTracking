c8afe8334e826876f08e9cf1653e159115378f25
Alexert97 <38686550+Alexert97@users.noreply.github.com>
2019-01-17 13:20:02 +0100

    fix: Popolazione testare prima di fare commit.

--- src/main/java/it/unisa/ErasmusTracking/util/popolazione.sql
+++ src/main/java/it/unisa/ErasmusTracking/util/popolazione.sql
@@ -3,7 +3,7 @@ insert into account (nome, cognome, e_mail, password, ruolo) Values ('Silvio', '
 insert into account (nome, cognome, e_mail, password, ruolo) Values ('Pascale', 'Capitone', 'pascale@gmail.com','root','studente');
 insert into account (nome, cognome, e_mail, password, ruolo) Values ('Carlo', 'Di Domenico', 'chardido@gmail.com','pizza','amministratore');
 
-insert into sendinginstitute (codice, dipartimento, indirizzo) Values ('15784', 'Unisa', 'Fisciano');
+insert into sendinginstitute (codice_erasmus, dipartimento, indirizzo) Values ('15784', 'Unisa', 'Fisciano');
 
 insert into coordinatore (sending_institute, account) Values (1, 1);
 insert into studente (matricola, data_nascita, luogo_nascita, sesso, nazionalita, telefono, ciclo_studi, anno_accademico, account, coordinatore) Values ('1234','12/12/12','Napoli', 'M', 'Italia', '377822', '1-triennale', 2, 2, 1);
@@ -11,4 +11,4 @@ insert into amministratore(account) Values (3);
 
 insert into location (citta, nazione, coordinatore, codice_erasmus) Values ('Roma', 'Polonia', 1, 054783);
 
-INSERT INTO `learningAgreement` (`id_learning_agreement`, `tipologiaErasmus`, `stato`, `livello_conoscenza_lingua`, `studente`) VALUES ('1', 'studio', 'passato', 'b1', '2');
+insert into learningAgreement (id_learning_agreement, tipologiaErasmus, stato, livello_conoscenza_lingua, studente) VALUES (1, 'studio', 'convalidato', 'A1', 2);
